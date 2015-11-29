ab8d760bccff5c0e65bbc7fe812e9d64dd8f1ccf
jidongdong <ronj2d@gmail.com>
2015-11-29 15:55:12 +0800

    EventBus使用Demo

--- app/src/main/java/com/putao/wd/me/address/AddressListActivity.java
+++ app/src/main/java/com/putao/wd/me/address/AddressListActivity.java
@@ -78,7 +78,19 @@ public class AddressListActivity extends PTWDActivity<GlobalApplication> impleme
                 Bundle bundle = new Bundle();
                 bundle.putBoolean(AddressListActivity.KEY_IS_ADD, true);
                 startActivity(AddressEditActivity.class, bundle);
+
+                //object为任何对象
+//                EventBusHelper.post(object,"自己定义的tag");
                 break;
         }
     }
+
+    //这里是接收post的方法.方法名可以自定义,o要与post的对象相同
+    //mode是表示处理逻辑在哪个线程里面执行,默认是在main线程
+    //ThreadMode.POST表示处理逻辑和EventBusHelper.post(object,"自己定义的tag")同一个线程
+    //ThreadMode.ASYNC表示不管EventBusHelper.post(object,"自己定义的tag")是不是异步线程,处理逻辑都会在新开的异步线程处理
+//    @Subcriber(tag = "自己定义的tag", mode = ThreadMode.MAIN)
+//    public void eventObject(Object o) {
+//        //处理逻辑
+//    }
 }
