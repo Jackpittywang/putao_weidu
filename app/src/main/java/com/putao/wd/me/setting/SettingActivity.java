0244656856c144d018a3afb6c1ac821573a7ec45
wangou <wangou@putao.com>
2015-12-01 21:00:37 +0800

    设置

--- app/src/main/java/com/putao/wd/me/setting/SettingActivity.java
+++ app/src/main/java/com/putao/wd/me/setting/SettingActivity.java
@@ -5,6 +5,9 @@ import android.view.View;
 import com.putao.wd.GlobalApplication;
 import com.putao.wd.R;
 import com.putao.wd.base.PTWDActivity;
+import com.putao.wd.me.address.AboutUsActivity;
+
+import butterknife.OnClick;
 
 /*
 **create by wangou
@@ -27,9 +30,13 @@ public class SettingActivity extends PTWDActivity<GlobalApplication> implements 
         return new String[0];
     }
 
+    @OnClick({R.id.si_about_us,R.id.si_modify_password})
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.si_about_us:startActivity(AboutUsActivity.class);break;
+            //case R.id.si_modify_password:startActivity(AboutUsActivity.class);break;
+        }
     }
 
 }
