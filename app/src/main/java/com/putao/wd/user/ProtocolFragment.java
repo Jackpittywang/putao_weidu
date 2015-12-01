7fcc60c6c5b79cb5969df3645eef39f063e0fe38
wangou <wangou@putao.com>
2015-12-01 15:37:15 +0800

    用户协议

--- app/src/main/java/com/putao/wd/user/ProtocolFragment.java
+++ app/src/main/java/com/putao/wd/user/ProtocolFragment.java
@@ -1,12 +1,20 @@
 package com.putao.wd.user;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import com.putao.wd.R;
 import com.sunnybear.library.controller.BasicFragment;
+import com.sunnybear.library.util.ResourcesUtils;
+
+import butterknife.Bind;
 
 
 public class ProtocolFragment extends BasicFragment {
+
+    @Bind(R.id.tv_protocol)
+    TextView tv_protocol;
 
     @Override
     protected int getLayoutId() {
@@ -15,8 +23,10 @@ public class ProtocolFragment extends BasicFragment {
 
     @Override
     public void onViewCreatedFinish(Bundle savedInstanceState) {
-
+        String protocol = ResourcesUtils.getAssetTextFile(mActivity, "protocol.txt");
+        tv_protocol.setText(Html.fromHtml(protocol));
     }
+
 
     @Override
     protected String[] getRequestUrls() {
