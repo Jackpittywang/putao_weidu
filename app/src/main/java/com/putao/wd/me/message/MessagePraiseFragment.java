eb88d30dc81a7467fe3384ff55e5ae7800c4cb1b
wangou <wangou@putao.com>
2015-12-02 14:47:42 +0800

    消息中心

--- app/src/main/java/com/putao/wd/me/message/MessagePraiseFragment.java
+++ app/src/main/java/com/putao/wd/me/message/MessagePraiseFragment.java
@@ -5,14 +5,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.putao.wd.R;
 import com.sunnybear.library.controller.BasicFragment;
+import com.sunnybear.library.view.recycler.BasicRecyclerView;
+
+import butterknife.Bind;
 
 /*
 **消息-赞
 **create by wangou
  */
 public class MessagePraiseFragment extends BasicFragment {
+    @Bind(R.id.tv_nomore)
+    TextView tv_nomore;//没有更多
+    @Bind(R.id.brv_msgprase)
+    BasicRecyclerView brv_msgprase;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_message_praise;
@@ -20,7 +30,10 @@ public class MessagePraiseFragment extends BasicFragment {
 
     @Override
     public void onViewCreatedFinish(Bundle savedInstanceState) {
-
+        if(1==0)//“赞”列表有数据
+            brv_msgprase.setVisibility(View.VISIBLE);
+        else
+            tv_nomore.setVisibility(View.VISIBLE);
     }
 
     @Override
--- app/src/main/java/com/putao/wd/me/message/MessageReplyFragment.java
+++ app/src/main/java/com/putao/wd/me/message/MessageReplyFragment.java
@@ -1,15 +1,24 @@
 package com.putao.wd.me.message;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.putao.wd.R;
 import com.sunnybear.library.controller.BasicFragment;
+import com.sunnybear.library.view.recycler.BasicRecyclerView;
+
+import butterknife.Bind;
 
 /*
 **消息-回复
 **create by wangou
  */
 public class MessageReplyFragment extends BasicFragment {
+    @Bind(R.id.tv_nomore)
+    TextView tv_nomore;//没有更多
+    @Bind(R.id.brv_msgreply)
+    BasicRecyclerView brv_msgreply;
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_message_reply;
@@ -17,7 +26,10 @@ public class MessageReplyFragment extends BasicFragment {
 
     @Override
     public void onViewCreatedFinish(Bundle savedInstanceState) {
-
+        if(1==0)//“赞”列表有数据
+            brv_msgreply.setVisibility(View.VISIBLE);
+        else
+            tv_nomore.setVisibility(View.VISIBLE);
     }
 
     @Override
