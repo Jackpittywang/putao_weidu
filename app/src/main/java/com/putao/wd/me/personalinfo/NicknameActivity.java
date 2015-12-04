8630f1ee1c5e92078af823318fba2000313be21d
wangou <wangou@putao.com>
2015-12-04 11:32:26 +0800

    我的用户昵称

---
+++ app/src/main/java/com/putao/wd/me/personalinfo/NicknameActivity.java
@@ -0,0 +1,15 @@
+package com.putao.wd.me.personalinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.putao.wd.R;
+
+public class NicknameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nickname);
+    }
+}
---
+++ app/src/main/java/com/putao/wd/me/personalinfo/PersonalInformationActivity.java
@@ -0,0 +1,48 @@
+package com.putao.wd.me.personalinfo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.putao.wd.GlobalApplication;
+import com.putao.wd.R;
+import com.putao.wd.base.PTWDActivity;
+import com.sunnybear.library.view.tab.TitleBar;
+
+import butterknife.OnClick;
+
+public class PersonalInformationActivity  extends PTWDActivity<GlobalApplication> implements View.OnClickListener {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_personal_information;
+    }
+
+    @Override
+    protected void onViewCreateFinish(Bundle saveInstanceState) {
+
+    }
+
+    @Override
+    protected String[] getRequestUrls() {
+        return new String[0];
+    }
+
+    @OnClick({R.id.si_personinfo_headicon,R.id.si_personinfo_nickname,R.id.si_personinfo_resume})
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.si_personinfo_headicon:
+
+                break;
+            case R.id.si_personinfo_nickname:
+
+                break;
+            case R.id.si_personinfo_resume:
+
+                break;
+
+
+        }
+    }
+
+}
