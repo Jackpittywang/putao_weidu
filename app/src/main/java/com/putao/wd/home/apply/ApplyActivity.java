a4f05cc6f74a33be0ad719b1c1b5156524ac22f1
wangou <wangou@putao.com>
2015-12-06 15:41:48 +0800

    我要参加

--- app/src/main/java/com/putao/wd/home/apply/ApplyActivity.java
+++ app/src/main/java/com/putao/wd/home/apply/ApplyActivity.java
@@ -2,10 +2,15 @@ package com.putao.wd.home.apply;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.facebook.imageformat.ImageFileExtensionMap;
 import com.putao.wd.GlobalApplication;
 import com.putao.wd.R;
 import com.putao.wd.base.PTWDActivity;
+
+import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * 我要参加
@@ -13,6 +18,10 @@ import com.putao.wd.base.PTWDActivity;
  *
  */
 public class ApplyActivity extends PTWDActivity<GlobalApplication> implements View.OnClickListener {
+    @Bind(R.id.iv_parentrelation_selecticon)
+    ImageView iv_parentrelation_selecticon;//家长身份
+    @Bind(R.id.iv_child_age)
+    ImageView iv_child_age;//孩子年龄
 
     @Override
     protected int getLayoutId() {
@@ -29,8 +38,16 @@ public class ApplyActivity extends PTWDActivity<GlobalApplication> implements Vi
         return new String[0];
     }
 
+    @OnClick({R.id.iv_parentrelation_selecticon,R.id.iv_child_age})
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.iv_parentrelation_selecticon://家长身份
 
+                break;
+            case R.id.iv_child_age://孩子年龄
+
+                break;
+        }
     }
 }
