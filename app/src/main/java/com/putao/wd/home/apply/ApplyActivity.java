9343ff9ce32a70007e97d372568523b6b8333223
wangou <wangou@putao.com>
2015-12-06 17:19:36 +0800

    我要参加

--- app/src/main/java/com/putao/wd/home/apply/ApplyActivity.java
+++ app/src/main/java/com/putao/wd/home/apply/ApplyActivity.java
@@ -1,5 +1,7 @@
 package com.putao.wd.home.apply;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -56,10 +58,52 @@ public class ApplyActivity extends PTWDActivity<GlobalApplication> implements Vi
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.iv_parentrelation_selecticon://家长身份
-
+                new AlertDialog.Builder(mContext).setTitle("年龄").setIcon(
+                        android.R.drawable.ic_dialog_info).setSingleChoiceItems(
+                        new String[] { "爷爷", "奶奶", "外公","外婆","爸爸","妈妈","叔叔","阿姨","舅舅","舅妈","姑姑","姑爹" }, 0,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which){
+                                    case 0:tv_parent_relation.setText("爷爷");break;
+                                    case 1:tv_parent_relation.setText("奶奶");break;
+                                    case 2:tv_parent_relation.setText("外公");break;
+                                    case 3:tv_parent_relation.setText("外婆");break;
+                                    case 4:tv_parent_relation.setText("爸爸");break;
+                                    case 5:tv_parent_relation.setText("妈妈");break;
+                                    case 6:tv_parent_relation.setText("叔叔");break;
+                                    case 7:tv_parent_relation.setText("阿姨");break;
+                                    case 8:tv_parent_relation.setText("舅舅");break;
+                                    case 9:tv_parent_relation.setText("舅妈");break;
+                                    case 10:tv_parent_relation.setText("姑姑");break;
+                                    case 11:tv_parent_relation.setText("姑爹");break;
+                                }
+                                dialog.dismiss();
+                            }
+                        }).setNegativeButton("取消", null).show();
                 break;
             case R.id.iv_child_age://孩子年龄
-
+                new AlertDialog.Builder(mContext).setTitle("年龄").setIcon(
+                        android.R.drawable.ic_dialog_info).setSingleChoiceItems(
+                        new String[] { "1岁", "2岁", "3岁","4岁","5岁","6岁","7岁","8岁","9岁","10岁","11岁","12岁" }, 0,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which){
+                                    case 0:tv_childage.setText("1岁");break;
+                                    case 1:tv_childage.setText("2岁");break;
+                                    case 2:tv_childage.setText("3岁");break;
+                                    case 3:tv_childage.setText("4岁");break;
+                                    case 4:tv_childage.setText("5岁");break;
+                                    case 5:tv_childage.setText("6岁");break;
+                                    case 6:tv_childage.setText("7岁");break;
+                                    case 7:tv_childage.setText("8岁");break;
+                                    case 8:tv_childage.setText("9岁");break;
+                                    case 9:tv_childage.setText("10岁");break;
+                                    case 10:tv_childage.setText("11岁");break;
+                                    case 11:tv_childage.setText("12岁");break;
+                                }
+                                dialog.dismiss();
+                            }
+                        }).setNegativeButton("取消", null).show();
                 break;
         }
     }
