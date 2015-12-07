5aec1d04c96520ff750b20aae882083c4158e7ae
wangou <wangou@putao.com>
2015-12-07 18:24:58 +0800

    填写订单

---
+++ app/src/main/java/com/putao/wd/store/order/WriteOrderActivity.java
@@ -0,0 +1,15 @@
+package com.putao.wd.store.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.putao.wd.R;
+
+public class WriteOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_order);
+    }
+}
