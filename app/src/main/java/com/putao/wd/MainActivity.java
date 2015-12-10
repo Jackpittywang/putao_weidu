9b70ed9f0b953b54b0f78bb4e9c8c8c2a6f189b7
jidongdong <ronj2d@gmail.com>
2015-12-10 20:21:56 +0800

    修改首页

--- app/src/main/java/com/putao/wd/MainActivity.java
+++ app/src/main/java/com/putao/wd/MainActivity.java
@@ -2,6 +2,7 @@ package com.putao.wd;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.SparseArray;
 import android.view.KeyEvent;
 
@@ -9,11 +10,10 @@ import com.putao.wd.home.MeFragment;
 import com.putao.wd.home.PutaoExploreFragment;
 import com.putao.wd.home.PutaoStartCircleFragment;
 import com.putao.wd.home.PutaoStoreFragment;
+import com.putao.wd.util.EmojiService;
 import com.sunnybear.library.controller.BasicFragmentActivity;
-import com.sunnybear.library.controller.FragmentSwitchAdapter;
 import com.sunnybear.library.view.select.TabBar;
 import com.sunnybear.library.view.select.TabItem;
-import com.sunnybear.library.view.viewpager.UnScrollableViewPager;
 
 import butterknife.Bind;
 
@@ -22,6 +22,7 @@ import butterknife.Bind;
  * Created by guchenkai on 2015/11/26.
  */
 public class MainActivity extends BasicFragmentActivity<GlobalApplication> implements TabBar.TabItemSelectedListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Bind(R.id.tb_tab)
     TabBar tb_tab;
     @Bind(R.id.ti_start_circle)
@@ -32,10 +33,13 @@ public class MainActivity extends BasicFragmentActivity<GlobalApplication> imple
     TabItem ti_store;
     @Bind(R.id.ti_me)
     TabItem ti_me;
-    @Bind(R.id.vp_content)
-    UnScrollableViewPager vp_content;
 
     private SparseArray<Fragment> mFragmentArray;
+
+    private PutaoStartCircleFragment mPutaoStartCircleFragment;//葡星圈
+    private PutaoExploreFragment mPutaoExploreFragment;//探索号
+    private PutaoStoreFragment mPutaoStoreFragment;//葡商城
+    private MeFragment mMeFragment;//我
 
     @Override
     protected int getLayoutId() {
@@ -44,40 +48,36 @@ public class MainActivity extends BasicFragmentActivity<GlobalApplication> imple
 
     @Override
     protected void onViewCreatedFinish(Bundle saveInstanceState) {
-        addFragment();
-        vp_content.setAdapter(new FragmentSwitchAdapter(getSupportFragmentManager(), mFragmentArray));
-        vp_content.setOffscreenPageLimit(4);
-        vp_content.setPageTransformer(false, null);
-
-        ti_start_circle.show(4);
-        ti_explore.show(4);
-        ti_store.show(4);
-        ti_me.show(4);
+        addHome();
+        ti_start_circle.show(4);//显示指示数字
+        ti_explore.show(4);//显示指示数字
+        ti_store.show(4);//显示指示数字
+        ti_me.show(4);//显示指示数字
         addListener();
-
-//        getSupportFragmentManager().beginTransaction()
-//                .add(R.id.fragment_container, mFragmentArray.get(R.id.ti_start_circle)).commit();
     }
 
-    private void addFragment() {
-        mFragmentArray = new SparseArray<>();
-        mFragmentArray.put(0, Fragment.instantiate(mContext, PutaoStartCircleFragment.class.getName()));
-        mFragmentArray.put(1, Fragment.instantiate(mContext, PutaoExploreFragment.class.getName()));
-        mFragmentArray.put(2, Fragment.instantiate(mContext, PutaoStoreFragment.class.getName()));
-        mFragmentArray.put(3, Fragment.instantiate(mContext, MeFragment.class.getName()));
+    /**
+     * 设置主页
+     */
+    private void addHome() {
+        mPutaoStartCircleFragment = (PutaoStartCircleFragment) Fragment.instantiate(mContext, PutaoStartCircleFragment.class.getName());
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, mPutaoStartCircleFragment).commit();
     }
 
-//    /**
-//     * 切换当前页
-//     *
-//     * @param resId
-//     */
-//    private void setCurrentItem(int resId) {
-//        Fragment fragment = mFragmentArray.get(resId);
-//        getSupportFragmentManager().beginTransaction()
-//                .replace(R.id.fragment_container, fragment)
-//                .addToBackStack(String.valueOf(resId)).commit();
-//    }
+    /**
+     * 隐藏fragment
+     */
+    private void hideFragment(FragmentTransaction fragmentTransaction) {
+        if (mPutaoStartCircleFragment != null)
+            fragmentTransaction.hide(mPutaoStartCircleFragment);
+        if (mPutaoExploreFragment != null)
+            fragmentTransaction.hide(mPutaoExploreFragment);
+        if (mPutaoStoreFragment != null)
+            fragmentTransaction.hide(mPutaoStoreFragment);
+        if (mMeFragment != null)
+            fragmentTransaction.hide(mMeFragment);
+    }
 
     /**
      * 添加监听器
@@ -98,26 +98,59 @@ public class MainActivity extends BasicFragmentActivity<GlobalApplication> imple
      */
     @Override
     public void onTabItemSelected(TabItem item, int position) {
-        int id = item.getId();
+        FragmentTransaction mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        hideFragment(mFragmentTransaction);
         switch (item.getId()) {
             case R.id.ti_start_circle://葡星圈
-                ti_start_circle.hide();
+                ti_start_circle.hide();//关闭指示数字
+                if (mPutaoStartCircleFragment == null) {
+                    mPutaoStartCircleFragment = (PutaoStartCircleFragment) Fragment.instantiate(mContext, PutaoStartCircleFragment.class.getName());
+                    mFragmentTransaction.add(R.id.fragment_container, mPutaoStartCircleFragment);
+                } else {
+                    mFragmentTransaction.show(mPutaoStartCircleFragment);
+                }
                 break;
             case R.id.ti_explore://探索号
-                ti_explore.hide();
+                ti_explore.hide();//关闭指示数字
+                if (mPutaoExploreFragment == null) {
+                    mPutaoExploreFragment = (PutaoExploreFragment) Fragment.instantiate(mContext, PutaoExploreFragment.class.getName());
+                    mFragmentTransaction.add(R.id.fragment_container, mPutaoExploreFragment);
+                } else {
+                    mFragmentTransaction.show(mPutaoExploreFragment);
+                }
                 break;
             case R.id.ti_store://葡商城
-                ti_store.hide();
+                ti_store.hide();//关闭指示数字
+                if (mPutaoStoreFragment == null) {
+                    mPutaoStoreFragment = (PutaoStoreFragment) Fragment.instantiate(mContext, PutaoStoreFragment.class.getName());
+                    mFragmentTransaction.add(R.id.fragment_container, mPutaoStoreFragment);
+                } else {
+                    mFragmentTransaction.show(mPutaoStoreFragment);
+                }
                 break;
             case R.id.ti_me://我
-                ti_me.hide();
-//                startService(CityService.class);
+                ti_me.hide();//关闭指示数字
+                if (mMeFragment == null) {
+                    mMeFragment = (MeFragment) Fragment.instantiate(mContext, MeFragment.class.getName());
+                    mFragmentTransaction.add(R.id.fragment_container, mMeFragment);
+                } else {
+                    mFragmentTransaction.show(mMeFragment);
+                }
+
+                startService(EmojiService.class);
                 startActivity(TestActivity.class);
                 break;
         }
-        vp_content.setCurrentItem(position, false);
+        mFragmentTransaction.commit();
     }
 
+    /**
+     * 返回键
+     *
+     * @param keyCode
+     * @param event
+     * @return
+     */
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK)
