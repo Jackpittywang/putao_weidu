201666abc5198253617d67fe394770faba162e65
wangou <wangou@putao.com>
2015-12-10 14:15:49 +0800

    售后列表

---
+++ app/src/main/java/com/putao/wd/me/sale/SaleListActivity.java
@@ -0,0 +1,162 @@
+package com.putao.wd.me.sale;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.putao.wd.GlobalApplication;
+import com.putao.wd.R;
+import com.putao.wd.base.PTWDActivity;
+import com.putao.wd.dto.OrderDto;
+import com.putao.wd.dto.OrderGoodsDto;
+import com.putao.wd.dto.OrderShipmentListItemDto;
+import com.putao.wd.me.order.OrderCommon;
+import com.putao.wd.me.order.OrderDetailActivity;
+import com.putao.wd.me.order.adapter.OrderListAdapter;
+import com.sunnybear.library.util.Logger;
+import com.sunnybear.library.util.ToastUtils;
+import com.sunnybear.library.view.recycler.BasicRecyclerView;
+import com.sunnybear.library.view.recycler.OnItemClickListener;
+import com.sunnybear.library.view.select.TitleBar;
+import com.sunnybear.library.view.select.TitleItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+
+
+/**
+ * 售后列表
+ * Created by wangou on 15/12/7.
+ */
+public class SaleListActivity extends PTWDActivity<GlobalApplication> implements View.OnClickListener {
+
+    @Bind(R.id.rv_order)
+    BasicRecyclerView rv_order;
+    @Bind(R.id.rl_no_order)
+    RelativeLayout rl_no_order;//没有order时的布局
+
+    @Bind(R.id.stickyHeaderLayout_sticky)
+    TitleBar ll_title;
+
+    private String TAG = SaleListActivity.class.getName();
+    private OrderListAdapter adapter;
+    private List<OrderDto> orderList;
+
+    // 当前第几个被选中
+    private int currentIndex = -1;
+    private List<Button> buttonList;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_sale_list;
+    }
+
+    @Override
+    protected void onViewCreatedFinish(Bundle saveInstanceState) {
+        addNavigation();
+
+        // 测试数据
+        orderList = new ArrayList<OrderDto>();
+
+        for (int i = 0; i < 10; i++) {
+            OrderDto order = new OrderDto();
+            order.setOrderNo(Math.random() + "");
+            order.setPurchaseTime(System.currentTimeMillis());
+            order.setTotalCost(569);
+            order.setCustomerName("葡萄科技");
+            order.setCustomerAddress("宜山路218号");
+            order.setCustomerPhone("13622222222");
+            if (i == 0) order.setOrderStatus(OrderCommon.ORDER_WAITING_PAY);
+            else if (i == 1) order.setOrderStatus(OrderCommon.ORDER_WAITING_SHIPMENT);
+            else if (i == 2) order.setOrderStatus(OrderCommon.ORDER_WAITING_SIGN);
+            else if (i == 3) order.setOrderStatus(OrderCommon.ORDER_SALE_SERVICE);
+            else if (i == 4) order.setOrderStatus(OrderCommon.ORDER_CANCLED);
+            else if (i == 5) order.setOrderStatus(OrderCommon.ORDER_CLOSED);
+            else order.setOrderStatus(OrderCommon.ORDER_WAITING_PAY);
+
+            int num = (int) (Math.random() * 3);
+            List<OrderGoodsDto> goodsList = new ArrayList<OrderGoodsDto>();
+            for (int j = 0; j < num; j++) {
+                OrderGoodsDto goods = new OrderGoodsDto();
+                goods.setImageUrl("http://bbs.putao.com/windid/attachment/avatar/000/66/22/662295_small.jpg");
+                goods.setNumber((int) Math.round(Math.random() * 10) + 1);
+                goods.setPrice((int) Math.round(Math.random() * 100) + 1);
+                goods.setName("葡萄探索号" + j);
+                goods.setSpecification("颜色：紫色");
+                if (j == 1) goods.setIsPreSale(true);
+                goodsList.add(goods);
+
+            }
+            order.setGoodsList(goodsList);
+
+            List<OrderShipmentListItemDto> shipmentList = new ArrayList<OrderShipmentListItemDto>();
+            num = (int) (Math.random() * 3) + 1;
+            Logger.i(TAG, "package num is:" + num);
+            for (int j = 0; j < num; j++) {
+                OrderShipmentListItemDto orderShipmentListItemDto = new OrderShipmentListItemDto();
+                orderShipmentListItemDto.setTitle("包裹" + j);
+                orderShipmentListItemDto.setStatus((int) (Math.random() * 7));
+                orderShipmentListItemDto.setShipmentId(System.currentTimeMillis() + "");
+                shipmentList.add(orderShipmentListItemDto);
+            }
+            order.setShipmentList(shipmentList);
+
+            orderList.add(order);
+
+        }
+        // 测试数据结束
+        if (orderList == null || orderList.size() == 0) {
+            rl_no_order.setVisibility(View.VISIBLE);
+            rv_order.setVisibility(View.INVISIBLE);
+            return;
+        } else {
+            rl_no_order.setVisibility(View.INVISIBLE);
+            rv_order.setVisibility(View.VISIBLE);
+        }
+
+        adapter = new OrderListAdapter(mContext, orderList);
+        rv_order.setAdapter(adapter);
+        //点击item
+        rv_order.setOnItemClickListener(new OnItemClickListener<OrderDto>() {
+
+            @Override
+            public void onItemClick(OrderDto orderDto, int position) {
+
+                Bundle bundle = new Bundle();
+                bundle.putSerializable(OrderDetailActivity.KEY_ORDER, orderDto);
+                startActivity(OrderDetailActivity.class, bundle);
+            }
+        });
+
+        refreshViewByType(0);
+    }
+
+    @Override
+    protected String[] getRequestUrls() {
+        return new String[0];
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
+    }
+
+
+
+
+    /**
+     * 根据类型刷新界面
+     *
+     * @param type
+     */
+    private void refreshViewByType(int type) {
+        Logger.i(TAG, "type clicked :" + type);
+    }
+
+
+}
