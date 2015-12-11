fa9ddf1254c5a9eb27d43cd0bb51e7bd5d35e555
yanghuaxian <123.com>
2015-12-11 10:53:12 +0800

    活动详情页添加接口，蒲星圈点击item进入活动祥情

--- app/src/main/java/com/putao/wd/home/PutaoExploreFragment.java
+++ app/src/main/java/com/putao/wd/home/PutaoExploreFragment.java
@@ -7,13 +7,13 @@ import android.util.Log;
 import com.putao.wd.R;
 import com.putao.wd.api.ExploreApi;
 import com.putao.wd.base.PTWDFragment;
-import com.putao.wd.dto.ExploreItem;
 import com.putao.wd.explore.manage.ManageActivity;
 import com.putao.wd.home.adapter.ExploreAdapter;
+import com.putao.wd.model.ExploreProduct;
 import com.putao.wd.qrcode.CaptureActivity;
 import com.sunnybear.library.model.http.callback.SimpleFastJsonCallback;
 import com.sunnybear.library.util.Logger;
-import com.sunnybear.library.view.recycler.BasicRecyclerView;
+import com.sunnybear.library.view.recycler.LoadMoreRecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +26,7 @@ import butterknife.Bind;
  */
 public class PutaoExploreFragment extends PTWDFragment {
     @Bind(R.id.rv_content)
-    BasicRecyclerView rv_content;
+    LoadMoreRecyclerView rv_content;
 
     private ExploreAdapter adapter;
 
@@ -39,7 +39,7 @@ public class PutaoExploreFragment extends PTWDFragment {
     public void onViewCreatedFinish(Bundle savedInstanceState) {
         addNavigation();
         setMainTitleColor(Color.WHITE);
-        adapter = new ExploreAdapter(mActivity, getTestData());
+        adapter = new ExploreAdapter(mActivity, null);
         rv_content.setAdapter(adapter);
     }
 
@@ -62,7 +62,12 @@ public class PutaoExploreFragment extends PTWDFragment {
      * 添加监听器
      */
     private void addListener(){
-        getExploreList(0, "", "", false, false);
+        rv_content.setOnLoadMoreListener(new LoadMoreRecyclerView.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                getExploreList(0, "", "", false, false);
+            }
+        });
     }
 
     /**
@@ -71,54 +76,54 @@ public class PutaoExploreFragment extends PTWDFragment {
      */
     private void getExploreList(int device_id, String start_time, String end_time, final boolean isRefresh, final boolean isLoadMore) {
         networkRequest(ExploreApi.getDiaryIndex(String.valueOf(device_id), start_time, end_time),
-                new SimpleFastJsonCallback<ArrayList<ExploreItem>>(ExploreItem.class, loading) {
-            @Override
-            public void onSuccess(String url, ArrayList<ExploreItem> result) {
-                Logger.d(result.toString());
-                Log.i("pt", "探索号请求成功");
-                //adapter.addAll(result);
-            }
-        });
+                new SimpleFastJsonCallback<ArrayList<ExploreProduct>>(ExploreProduct.class, loading) {
+                    @Override
+                    public void onSuccess(String url, ArrayList<ExploreProduct> result) {
+                        Logger.d(result.toString());
+                        Log.i("pt", "探索号请求成功");
+                        //adapter.addAll(result);
+                    }
+                });
     }
 
+//    private List<ExploreItem> getTestData() {
+//        List<ExploreItem> list = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            ExploreItem exploreItem = new ExploreItem();
+//            if (i == 1 || i == 3 || i == 5) {
+//                exploreItem.setIsMixed(true);
+//                switch (i) {
+//                    case 1:
+//                        exploreItem.setIconNum(1);
+//                        break;
+//                    case 3:
+//                        exploreItem.setIconNum(4);
+//                        break;
+//                    case 5:
+//                        exploreItem.setIconNum(9);
+//                        break;
+//                    default:
+//                        break;
+//                }
+//            } else {
+//                exploreItem.setIsMixed(false);
+//            }
+//            if (i < 4) {
+//                exploreItem.setDate("2015-12-10");
+//            } else if (i >= 4 && i < 7) {
+//                exploreItem.setDate("2015-12-09");
+//            } else {
+//                exploreItem.setDate("2015-12-08");
+//            }
+//            exploreItem.setSkill_name("技能" + i);
+//            exploreItem.setContent("这里显示每个关卡背后的教育理念的主要文字，大概是一句话左右。" +
+//                    "完成多个关卡，则以分号隔开显示，一直向下展示。这里显示每个关卡背后的教育理念的主要文字，" +
+//                    "大概是一句话左右。完成多个关卡，则以分号隔开显示，一直向下展示。" + i);
+//            list.add(exploreItem);
+//        }
+//        return list;
+//    }
 
-    private List<ExploreItem> getTestData() {
-        List<ExploreItem> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            ExploreItem exploreItem = new ExploreItem();
-            if (i == 1 || i == 3 || i == 5) {
-                exploreItem.setIsMixed(true);
-                switch (i) {
-                    case 1:
-                        exploreItem.setIconNum(1);
-                        break;
-                    case 3:
-                        exploreItem.setIconNum(4);
-                        break;
-                    case 5:
-                        exploreItem.setIconNum(9);
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                exploreItem.setIsMixed(false);
-            }
-            if (i < 4) {
-                exploreItem.setDate("2015-12-10");
-            } else if (i >= 4 && i < 7) {
-                exploreItem.setDate("2015-12-09");
-            } else {
-                exploreItem.setDate("2015-12-08");
-            }
-            exploreItem.setSkill_name("技能" + i);
-            exploreItem.setContent("这里显示每个关卡背后的教育理念的主要文字，大概是一句话左右。" +
-                    "完成多个关卡，则以分号隔开显示，一直向下展示。这里显示每个关卡背后的教育理念的主要文字，" +
-                    "大概是一句话左右。完成多个关卡，则以分号隔开显示，一直向下展示。" + i);
-            list.add(exploreItem);
-        }
-        return list;
-    }
 }
 
 
