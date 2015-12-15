cc1d1b827c8054f49e56beca6242f3a7faea9be0
wangou <wangou@putao.com>
2015-12-15 17:22:20 +0800

    订单

--- app/src/main/java/com/putao/wd/base/PTWDRequestHelper.java
+++ app/src/main/java/com/putao/wd/base/PTWDRequestHelper.java
@@ -23,14 +23,11 @@ public class PTWDRequestHelper {
      * @return Request实例
      */
     public static FormEncodingRequestBuilder store() {
-//        测试购物车
+
         return FormEncodingRequestBuilder.newInstance()
-                .addParam(PTWDRequestHelper.REQUEST_KEY_UID, "60000277")
-                .addParam(PTWDRequestHelper.REQUEST_KEY_TOKEN, "e8504333dfaa482488c99f3322fd3df3");
-//        测试订单
-//        return FormEncoding   RequestBuilder.newInstance()
-//                .addParam(PTWDRequestHelper.REQUEST_KEY_UID, "60000277")
-//                .addParam(PTWDRequestHelper.REQUEST_KEY_TOKEN, "4907402e527346898fce328ecededc4a");
+                .addParam(PTWDRequestHelper.REQUEST_KEY_UID, PreferenceUtils.getValue(GlobalApplication.PREFERENCE_KEY_UID, ""))
+                .addParam(PTWDRequestHelper.REQUEST_KEY_TOKEN, PreferenceUtils.getValue(GlobalApplication.PREFERENCE_KEY_TOKEN, ""));
+
     }
 
     /**
