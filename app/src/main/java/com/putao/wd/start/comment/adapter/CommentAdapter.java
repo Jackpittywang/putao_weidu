d96ae94d2ea1dea97a085b81dc18c4aae35240bc
yanghuaxian <123.com>
2015-12-16 14:24:22 +0800

    评论表情添加

--- app/src/main/java/com/putao/wd/start/comment/adapter/CommentAdapter.java
+++ app/src/main/java/com/putao/wd/start/comment/adapter/CommentAdapter.java
@@ -1,11 +1,13 @@
 package com.putao.wd.start.comment.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 
 import com.putao.wd.R;
 import com.putao.wd.model.Comment;
+import com.sunnybear.library.eventbus.EventBusHelper;
 import com.sunnybear.library.util.DateUtils;
 import com.sunnybear.library.view.SwitchButton;
 import com.sunnybear.library.view.image.ImageDraweeView;
@@ -21,6 +23,7 @@ import butterknife.Bind;
  * Created by yanghx on 2015/12/8.
  */
 public class CommentAdapter extends LoadMoreAdapter<Comment, CommentAdapter.CommentViewHolder> {
+    public static final String EVENT_COMMENT_EDIT = "event_comment_edit";
     private final String DATE_PATTERN = "yyyy-MM-dd";//格式化时间戳规则
 
 
@@ -39,12 +42,19 @@ public class CommentAdapter extends LoadMoreAdapter<Comment, CommentAdapter.Comm
     }
 
     @Override
-    public void onBindItem(final CommentViewHolder holder, final Comment comment, int position) {
+    public void onBindItem(final CommentViewHolder holder, final Comment comment, final int position) {
         holder.iv_comment_icon.setImageURL(comment.getUser_profile_photo());
         holder.tv_username.setText(comment.getUser_name());
         String create_time = DateUtils.secondToDate(Integer.parseInt(comment.getCreate_time()), DATE_PATTERN);
         holder.tv_comment_time.setText(create_time);
         holder.tv_comment_content.setText(comment.getContent());
+        holder.tv_count_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //使用EventBus控制表情栏弹出
+                EventBusHelper.post(position, EVENT_COMMENT_EDIT);
+            }
+        });
 //        holder.tv_count_comment.setText("1234");
         if (!"0".equals(comment.getCount_cool())) {
             holder.tv_count_cool.setText(comment.getCount_cool());
