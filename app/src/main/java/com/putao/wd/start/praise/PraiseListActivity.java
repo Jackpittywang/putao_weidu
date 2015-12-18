fa714af4e232dbcbb01bff63214f8773075748d2
yanghuaxian <123.com>
2015-12-18 16:05:59 +0800

    赞列表 报名列表相关调整

--- app/src/main/java/com/putao/wd/start/praise/PraiseListActivity.java
+++ app/src/main/java/com/putao/wd/start/praise/PraiseListActivity.java
@@ -28,52 +28,73 @@ import butterknife.Bind;
  * Created by wangou on 2015/12/4.
  */
 public class PraiseListActivity extends PTWDActivity<GlobalApplication> implements View.OnClickListener {
-    @Bind(R.id.brv_praiselist)
-    LoadMoreRecyclerView brv_praiselist;
+    @Bind(R.id.rv_praiselist)
+    LoadMoreRecyclerView rv_praiselist;
     @Bind(R.id.tv_nomore)
     TextView tv_nomore;
     @Bind(R.id.ll_praiselist)
     LinearLayout ll_praiselist;
 
     private PraiseListAdapter adapter;
+    private String action_id;
+
+    @Override
+    protected void onViewCreatedFinish(Bundle saveInstanceState) {
+        addNavigation();
+
+        adapter = new PraiseListAdapter(mContext, null);
+        rv_praiselist.setAdapter(adapter);
+        Bundle bundle = getIntent().getExtras();
+        action_id = bundle.getString("action_id");
+
+        getCoolList();
+        addListener();
+    }
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_praise_list;
     }
 
-    @Override
-    protected void onViewCreatedFinish(Bundle saveInstanceState) {
-        addNavigation();
-        addListener();
+    private void addListener() {
+        rv_praiselist.setOnLoadMoreListener(new LoadMoreRecyclerView.OnLoadMoreListener() {
+            public void onLoadMore() {
+                (new Handler()).postDelayed(new Runnable() {
+                    public void run() {
+                        getCoolList();
+                    }
+                }, 3000L);
+            }
+        });
+    }
 
-        adapter = new PraiseListAdapter(mContext, null);
-        brv_praiselist.setAdapter(adapter);
-
-        Bundle bundle = getIntent().getExtras();
-        String action_id = bundle.getString("action_id");
+    /**
+     * 获取赞列表
+     */
+    private void getCoolList() {
         networkRequest(StartApi.getCoolList(action_id), new SimpleFastJsonCallback<Cool>(Cool.class, loading) {
             @Override
             public void onSuccess(String url, Cool result) {
-                //Logger.i("mmmmmmmmmm"+result.toString());
-                ArrayList<CoolList> user_list = result.getUser_list();
-                adapter.replaceAll(user_list);
                 Logger.i("赞列表获取成功");
+                ArrayList<CoolList> user_list = result.getUser_list();
+                if (user_list.size() != 0) {
+                    adapter.replaceAll(user_list);
+                } else {
+                    rv_praiselist.noMoreLoading();
+                }
                 loading.dismiss();
             }
         });
     }
 
-    private void addListener() {
-        this.brv_praiselist.setOnLoadMoreListener(new LoadMoreRecyclerView.OnLoadMoreListener() {
-            public void onLoadMore() {
-                (new Handler()).postDelayed(new Runnable() {
-                    public void run() {
-                        brv_praiselist.noMoreLoading();
-                    }
-                }, 3000L);
-            }
-        });
+    @Override
+    protected String[] getRequestUrls() {
+        return new String[0];
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 
 //    private void initPraiseList(){
@@ -88,36 +109,27 @@ public class PraiseListActivity extends PTWDActivity<GlobalApplication> implemen
 //        }
 //    }
 
-    private List<PraiseListItem> initPraiseListData(){
-        List<PraiseListItem> praiseList=new ArrayList<>();
-        PraiseListItem item;
+//    private List<PraiseListItem> initPraiseListData(){
+//        List<PraiseListItem> praiseList=new ArrayList<>();
+//        PraiseListItem item;
+//
+//        for (int i = 0; i < 20; i++) {
+//            item=new PraiseListItem();
+//            item.setUserIconUrl("https://static.pexels.com/photos/5854/sea-woman-legs-water-medium.jpg");
+//            item.setUsername("username" + i);
+//            item.setPraiseCount(i+"");
+//            if(i%2==0)
+//                item.setUserattr("游客");
+//            else if(i==9){
+//                item.setUserattr("用户本人");
+//            }
+//            else{
+//                item.setUserattr("普通用户");
+//            }
+//            praiseList.add(item);
+//        }
+//
+//        return praiseList;
+//    }
 
-        for (int i = 0; i < 20; i++) {
-            item=new PraiseListItem();
-            item.setUserIconUrl("https://static.pexels.com/photos/5854/sea-woman-legs-water-medium.jpg");
-            item.setUsername("username" + i);
-            item.setPraiseCount(i+"");
-            if(i%2==0)
-                item.setUserattr("游客");
-            else if(i==9){
-                item.setUserattr("用户本人");
-            }
-            else{
-                item.setUserattr("普通用户");
-            }
-            praiseList.add(item);
-        }
-
-        return praiseList;
-    }
-
-    @Override
-    protected String[] getRequestUrls() {
-        return new String[0];
-    }
-
-    @Override
-    public void onClick(View v) {
-
-    }
 }
