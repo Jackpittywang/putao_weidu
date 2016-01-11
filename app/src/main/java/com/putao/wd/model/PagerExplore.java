3b13c76438d531461013ec678ce7183234f8d1ca
yanghuaxian <123.com>
2016-01-11 16:54:31 +0800

    探索（首页）

---
+++ app/src/main/java/com/putao/wd/model/PagerExplore.java
@@ -0,0 +1,16 @@
+package com.putao.wd.model;
+
+import java.io.Serializable;
+
+/**
+ * 探索--ViewPager数据
+ * Created by yanghx on 2016/1/11.
+ */
+public class PagerExplore implements Serializable {
+
+
+
+
+
+
+}
