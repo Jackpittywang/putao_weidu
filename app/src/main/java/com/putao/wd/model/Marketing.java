1074cc917c7031d6c77085c09a68068130ff1300
zhanghao <zhanghao@putao.com>
2016-01-11 12:45:26 +0800

    创想界面

---
+++ app/src/main/java/com/putao/wd/model/Marketing.java
@@ -0,0 +1,12 @@
+package com.putao.wd.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 活动详情
+ * Created by guchenkai on 2015/12/8.
+ */
+public class Marketing implements Serializable {
+
+}
