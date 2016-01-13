d6a05ad7c3ba73acebdc14581692c4566d1f71e8
yanghuaxian <123.com>
2016-01-13 14:07:02 +0800

    探索（首页）

--- app/src/main/java/com/putao/wd/explore/ExploreDetailFragment.java
+++ app/src/main/java/com/putao/wd/explore/ExploreDetailFragment.java
@@ -16,24 +16,24 @@ import butterknife.Bind;
  * Created by yanghx on 2016/1/13.
  */
 public class ExploreDetailFragment extends BasicFragment {
-    @Bind(R.id.iv_close)
-    ImageView iv_close;
-    @Bind(R.id.iv_top)
-    ImageView iv_top;
-    @Bind(R.id.iv_user_icon)
-    ImageView iv_user_icon;
-    @Bind(R.id.tv_title)
-    TextView tv_title;
-    @Bind(R.id.tv_digest)
-    TextView tv_digest;
-    @Bind(R.id.web)
-    WebView web;
-    @Bind(R.id.ll_cool)
-    LinearLayout ll_cool;
-    @Bind(R.id.ll_comment)
-    LinearLayout ll_comment;
-    @Bind(R.id.ll_share)
-    LinearLayout ll_share;
+//    @Bind(R.id.iv_close)
+//    ImageView iv_close;
+//    @Bind(R.id.iv_top)
+//    ImageView iv_top;
+//    @Bind(R.id.iv_user_icon)
+//    ImageView iv_user_icon;
+//    @Bind(R.id.tv_title)
+//    TextView tv_title;
+//    @Bind(R.id.tv_digest)
+//    TextView tv_digest;
+//    @Bind(R.id.web)
+//    WebView web;
+//    @Bind(R.id.ll_cool)
+//    LinearLayout ll_cool;
+//    @Bind(R.id.ll_comment)
+//    LinearLayout ll_comment;
+//    @Bind(R.id.ll_share)
+//    LinearLayout ll_share;
 
 
     @Override
