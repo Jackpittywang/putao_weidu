c76184654d7d2a44b69603eff28c06eeb11da903
zhanghao <zhanghao@putao.com>
2016-01-15 14:01:20 +0800

    创造详情

--- app/src/main/java/com/putao/wd/created/CreatedDetailActivity.java
+++ app/src/main/java/com/putao/wd/created/CreatedDetailActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import com.putao.wd.R;
 import com.putao.wd.base.PTWDActivity;
+import com.putao.wd.home.adapter.CreateAdapter;
+import com.putao.wd.model.Marketing;
 import com.sunnybear.library.view.BasicWebView;
+import com.sunnybear.library.view.recycler.BasicRecyclerView;
 
 import butterknife.Bind;
 
@@ -15,6 +18,9 @@ import butterknife.Bind;
 public class CreatedDetailActivity extends PTWDActivity {
     @Bind(R.id.wv_content)
     BasicWebView wv_content;
+    @Bind(R.id.rv_created_correlation)
+    BasicRecyclerView rv_created_correlation;
+    private CreateAdapter adapter;
 
     @Override
     protected int getLayoutId() {
@@ -25,6 +31,11 @@ public class CreatedDetailActivity extends PTWDActivity {
     protected void onViewCreatedFinish(Bundle saveInstanceState) {
         addNavigation();
         wv_content.loadUrl("http://3g.qq.com");
+        adapter = new CreateAdapter(mContext, null);
+        rv_created_correlation.setAdapter(adapter);
+        adapter.add(new Marketing());
+        adapter.add(new Marketing());
+        adapter.add(new Marketing());
     }
 
     @Override
