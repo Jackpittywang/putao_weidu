6ac27d2e222462df8b01147f67c385cfb8f6b4e9
jidongdong <ronj2d@gmail.com>
2016-01-15 15:02:53 +0800

    修改创造详情底部相关创造

--- app/src/main/java/com/putao/wd/created/CreatedDetailActivity.java
+++ app/src/main/java/com/putao/wd/created/CreatedDetailActivity.java
@@ -7,19 +7,19 @@ import com.putao.wd.base.PTWDActivity;
 import com.putao.wd.home.adapter.CreateAdapter;
 import com.putao.wd.model.Marketing;
 import com.sunnybear.library.view.BasicWebView;
-import com.sunnybear.library.view.recycler.BasicRecyclerView;
+import com.sunnybear.library.view.recycler.LoadMoreRecyclerView;
 
 import butterknife.Bind;
 
 /**
- * 创想详情
+ * 创造详情
  * Created by guchenkai on 2016/1/11.
  */
 public class CreatedDetailActivity extends PTWDActivity {
     @Bind(R.id.wv_content)
     BasicWebView wv_content;
     @Bind(R.id.rv_created_correlation)
-    BasicRecyclerView rv_created_correlation;
+    LoadMoreRecyclerView rv_created_correlation;
     private CreateAdapter adapter;
 
     @Override
--- library/src/main/java/com/sunnybear/library/view/recycler/InnerLinearLayoutManager.java
+++ library/src/main/java/com/sunnybear/library/view/recycler/InnerLinearLayoutManager.java
@@ -27,49 +27,61 @@ public class InnerLinearLayoutManager extends LinearLayoutManager {
 
     @Override
     public void onMeasure(RecyclerView.Recycler recycler, RecyclerView.State state, int widthSpec, int heightSpec) {
-
-        final int widthMode = View.MeasureSpec.getMode(widthSpec);
-        final int heightMode = View.MeasureSpec.getMode(heightSpec);
-        final int widthSize = View.MeasureSpec.getSize(widthSpec);
-        final int heightSize = View.MeasureSpec.getSize(heightSpec);
-        Logger.i(TAG, "onMeasure called. \nwidthMode " + widthMode
-                + " \nheightMode " + heightSpec
-                + " \nwidthSize " + widthSize
-                + " \nheightSize " + heightSize
-                + " \ngetItemCount() " + getItemCount());
-
-        int width = 0;
         int height = 0;
-        for (int i = 0; i < getItemCount(); i++) {
-            measureScrapChild(recycler, i,
-                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
-                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
-                    mMeasuredDimension);
-
-            if (getOrientation() == HORIZONTAL) {
-                width = width + mMeasuredDimension[0];
-                if (i == 0)
-                    height = mMeasuredDimension[1];
-            } else {
-                height = height + mMeasuredDimension[1];
-                if (i == 0)
-                    width = mMeasuredDimension[0];
-            }
+        Logger.i("onMeasure---MeasureSpec-" + View.MeasureSpec.getSize(heightSpec));
+        int childCount = getItemCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = recycler.getViewForPosition(i);
+            measureChild(child, widthSpec, heightSpec);
+//            if (i % getSpanCount() == 0) {
+                int measuredHeight = child.getMeasuredHeight()/* + getDecoratedBottom(child)*/;
+                height += measuredHeight;
+//            }
         }
-        switch (widthMode) {
-            case View.MeasureSpec.EXACTLY:
-                width = widthSize;
-            case View.MeasureSpec.AT_MOST:
-            case View.MeasureSpec.UNSPECIFIED:
-        }
-
-        switch (heightMode) {
-            case View.MeasureSpec.EXACTLY:
-                height = heightSize;
-            case View.MeasureSpec.AT_MOST:
-            case View.MeasureSpec.UNSPECIFIED:
-        }
-        setMeasuredDimension(width, height);
+        Logger.i("onMeasure---height-" + height);
+        setMeasuredDimension(View.MeasureSpec.getSize(widthSpec), height);
+//        final int widthMode = View.MeasureSpec.getMode(widthSpec);
+//        final int heightMode = View.MeasureSpec.getMode(heightSpec);
+//        final int widthSize = View.MeasureSpec.getSize(widthSpec);
+//        final int heightSize = View.MeasureSpec.getSize(heightSpec);
+//        Logger.i(TAG, "onMeasure called. \nwidthMode " + widthMode
+//                + " \nheightMode " + heightSpec
+//                + " \nwidthSize " + widthSize
+//                + " \nheightSize " + heightSize
+//                + " \ngetItemCount() " + getItemCount());
+//
+//        int width = 0;
+//        int height = 0;
+//        for (int i = 0; i < getItemCount(); i++) {
+//            measureScrapChild(recycler, i,
+//                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+//                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+//                    mMeasuredDimension);
+//
+//            if (getOrientation() == HORIZONTAL) {
+//                width = width + mMeasuredDimension[0];
+//                if (i == 0)
+//                    height = mMeasuredDimension[1];
+//            } else {
+//                height = height + mMeasuredDimension[1];
+//                if (i == 0)
+//                    width = mMeasuredDimension[0];
+//            }
+//        }
+//        switch (widthMode) {
+//            case View.MeasureSpec.EXACTLY:
+//                width = widthSize;
+//            case View.MeasureSpec.AT_MOST:
+//            case View.MeasureSpec.UNSPECIFIED:
+//        }
+//
+//        switch (heightMode) {
+//            case View.MeasureSpec.EXACTLY:
+//                height = heightSize;
+//            case View.MeasureSpec.AT_MOST:
+//            case View.MeasureSpec.UNSPECIFIED:
+//        }
+//        setMeasuredDimension(width, height);
     }
 
     private void measureScrapChild(RecyclerView.Recycler recycler, int position, int widthSpec, int heightSpec, int[] measuredDimension) {
