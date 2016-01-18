8791c4dd28a7a4d9bf658cf6b2071c9c07d38048
guck@shangmengit.com <sunnybear@foxmail.com>
2016-01-18 13:08:42 +0800

    设置无网络拦截器

--- library/src/main/java/com/sunnybear/library/BasicApplication.java
+++ library/src/main/java/com/sunnybear/library/BasicApplication.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.squareup.okhttp.OkHttpClient;
 import com.sunnybear.library.model.http.OkHttpManager;
+import com.sunnybear.library.model.http.interceptor.NetworkInterceptor;
 import com.sunnybear.library.util.AppUtils;
 import com.sunnybear.library.util.CrashHandler;
 import com.sunnybear.library.util.DiskFileCacheHelper;
@@ -58,7 +59,8 @@ public abstract class BasicApplication extends Application {
         //日志输出
         Logger.init(getLogTag()).hideThreadInfo().setLogLevel(isDebug() ? Logger.LogLevel.FULL : Logger.LogLevel.NONE);
         //OkHttp初始化
-        mOkHttpClient = OkHttpManager.getInstance(getNetworkCacheDirectoryPath(), getNetworkCacheSize()).build();
+        mOkHttpClient = OkHttpManager.getInstance(getNetworkCacheDirectoryPath(), getNetworkCacheSize())
+                .add(new NetworkInterceptor()).build();
         //Fresco初始化
         Fresco.initialize(getApplicationContext(),
                 ImagePipelineConfigFactory.getOkhttpImagePipelineConfig(getApplicationContext(), mOkHttpClient));
---
+++ library/src/main/java/com/sunnybear/library/model/http/interceptor/NetworkInterceptor.java
@@ -0,0 +1,28 @@
+package com.sunnybear.library.model.http.interceptor;
+
+import com.squareup.okhttp.Interceptor;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.sunnybear.library.BasicApplication;
+import com.sunnybear.library.R;
+import com.sunnybear.library.util.AppUtils;
+import com.sunnybear.library.util.ToastUtils;
+
+import java.io.IOException;
+
+/**
+ * 判断是否有网络
+ * Created by sunnybear on 16/1/18.
+ */
+public class NetworkInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();//拦截Request
+        if (!AppUtils.isNetworkReachable(BasicApplication.getInstance())) {
+            ToastUtils.showToastLong(BasicApplication.getInstance(), BasicApplication.getInstance().getResources().getString(R.string.not_network));
+            return null;
+        }
+        return chain.proceed(request);
+    }
+}
