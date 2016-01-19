56a223003c21dade89bd7734a79e384f0ae7756e
jidongdong <ronj2d@gmail.com>
2016-01-19 10:37:41 +0800

    添加阴影控件

--- app/src/main/java/com/putao/wd/created/CreatedDetailActivity.java
+++ app/src/main/java/com/putao/wd/created/CreatedDetailActivity.java
@@ -5,11 +5,8 @@ import android.os.Bundle;
 import com.alibaba.fastjson.JSONObject;
 import com.putao.wd.R;
 import com.putao.wd.base.PTWDActivity;
-import com.putao.wd.created.adapter.CreateDetailAdapter;
-import com.putao.wd.model.Marketing;
 import com.sunnybear.library.util.Logger;
 import com.sunnybear.library.view.BasicWebView;
-import com.sunnybear.library.view.recycler.BasicRecyclerView;
 
 import butterknife.Bind;
 
@@ -18,12 +15,8 @@ import butterknife.Bind;
  * Created by guchenkai on 2016/1/11.
  */
 public class CreatedDetailActivity extends PTWDActivity {
-
     @Bind(R.id.wv_content)
     BasicWebView wv_content;
-    @Bind(R.id.rv_created_correlation)
-    BasicRecyclerView rv_created_correlation;
-    private CreateDetailAdapter adapter;
 
     @Override
     protected int getLayoutId() {
@@ -35,8 +28,6 @@ public class CreatedDetailActivity extends PTWDActivity {
         addNavigation();
 
         wv_content.loadUrl("http://3g.qq.com");
-        adapter = new CreateDetailAdapter(mContext, null);
-        rv_created_correlation.setAdapter(adapter);
 
         addListener();
     }
@@ -51,10 +42,6 @@ public class CreatedDetailActivity extends PTWDActivity {
             @Override
             public void onWebPageLoaderFinish(String url) {
                 Logger.d("网页加载完成");
-
-                adapter.add(new Marketing());
-                adapter.add(new Marketing());
-                adapter.add(new Marketing());
             }
         });
     }
