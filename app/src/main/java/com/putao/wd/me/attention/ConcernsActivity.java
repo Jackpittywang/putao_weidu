5ce1d1ef4536419a68c457ccdec837fcab01d5e5
jidongdong <ronj2d@gmail.com>
2016-02-29 20:27:40 +0800

    关注空页面

--- app/src/main/java/com/putao/wd/me/attention/ConcernsActivity.java
+++ app/src/main/java/com/putao/wd/me/attention/ConcernsActivity.java
@@ -1,16 +1,16 @@
 package com.putao.wd.me.attention;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.putao.wd.R;
 import com.putao.wd.api.CreateApi;
 import com.putao.wd.base.PTWDActivity;
 import com.putao.wd.created.CreateBasicDetailActivity;
-import com.putao.wd.created.FancyDetailActivity;
 import com.putao.wd.created.adapter.FancyAdapter;
 import com.putao.wd.model.Create;
 import com.putao.wd.model.Creates;
-import com.putao.wd.model.Marketing;
 import com.sunnybear.library.controller.eventbus.Subcriber;
 import com.sunnybear.library.model.http.callback.SimpleFastJsonCallback;
 import com.sunnybear.library.util.Logger;
@@ -18,7 +18,7 @@ import com.sunnybear.library.view.PullToRefreshLayout;
 import com.sunnybear.library.view.recycler.LoadMoreRecyclerView;
 import com.sunnybear.library.view.recycler.listener.OnItemClickListener;
 
-import java.io.Serializable;
+import java.util.List;
 
 import butterknife.Bind;
 
@@ -31,6 +31,8 @@ public class ConcernsActivity extends PTWDActivity implements PullToRefreshLayou
     LoadMoreRecyclerView rv_created;
     @Bind(R.id.ptl_refresh)
     PullToRefreshLayout ptl_refresh;
+    @Bind(R.id.tv_message_empty)
+    TextView tv_message_empty;
 
     private boolean isRefresh = false;
     private FancyAdapter adapter;
@@ -45,6 +47,7 @@ public class ConcernsActivity extends PTWDActivity implements PullToRefreshLayou
     public void onViewCreatedFinish(Bundle saveInstanceState) {
         Logger.d("PutaoCreatedFragment启动");
         addNavigation();
+        tv_message_empty.setText("还没有关注");
         adapter = new FancyAdapter(mContext, null);
         rv_created.setAdapter(adapter);
         initData();
@@ -57,10 +60,21 @@ public class ConcernsActivity extends PTWDActivity implements PullToRefreshLayou
                 new SimpleFastJsonCallback<Creates>(Creates.class, loading) {
                     @Override
                     public void onSuccess(String url, Creates result) {
-                        adapter.replaceAll(result.getData());
+                        List<Create> details = result.getData();
+                        if (details != null && details.size() > 0 ) {
+                            tv_message_empty.setVisibility(View.GONE);
+                            ptl_refresh.setVisibility(View.VISIBLE);
+                            adapter.addAll(details);
+                        }else{
+                            ptl_refresh.setVisibility(View.GONE);
+                            tv_message_empty.setVisibility(View.VISIBLE);
+                        }
+                        loading.dismiss();
+
+                        /*adapter.replaceAll(result.getData());
                         ptl_refresh.refreshComplete();
                         checkLoadMoreComplete(result.getCurrentPage(), result.getTotalPage());
-                        loading.dismiss();
+                        loading.dismiss();*/
                     }
                 });
     }
