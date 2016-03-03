019551232fe3ee3b60aa77538a478e02906fada6
jidongdong <ronj2d@gmail.com>
2016-03-03 16:00:49 +0800

    回复刷新

--- app/src/main/java/com/putao/wd/me/message/ReplyFragment.java
+++ app/src/main/java/com/putao/wd/me/message/ReplyFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import com.putao.wd.R;
 import com.putao.wd.api.StartApi;
 import com.putao.wd.me.message.adapter.ReplyAdapter;
+import com.putao.wd.model.Praise;
 import com.putao.wd.model.Reply;
 import com.putao.wd.model.ReplyDetail;
 import com.sunnybear.library.controller.BasicFragment;
@@ -34,7 +35,7 @@ public class ReplyFragment extends BasicFragment {
 
     private ReplyAdapter adapter;
 
-    private int currentPage = 1;
+    private int mPage;
 
     @Override
     protected int getLayoutId() {
@@ -47,9 +48,9 @@ public class ReplyFragment extends BasicFragment {
         tv_message_empty.setText("还没有回复");
         adapter = new ReplyAdapter(mActivity, null);
         rv_content.setAdapter(adapter);
-        addListener();
-
         getNotifyList();
+
+        addListener();
     }
 
     /**
@@ -59,7 +60,17 @@ public class ReplyFragment extends BasicFragment {
         rv_content.setOnLoadMoreListener(new LoadMoreRecyclerView.OnLoadMoreListener() {
             @Override
             public void onLoadMore() {
-                getNotifyList();
+                //getNotifyList();
+                networkRequest(StartApi.getReplyList(String.valueOf(mPage)),
+                        new SimpleFastJsonCallback<Reply>(Praise.class, loading) {
+                            @Override
+                            public void onSuccess(String url, Reply result) {
+                                adapter.addAll(result.getReply());
+                                rv_content.loadMoreComplete();
+                                checkLoadMoreComplete(result.getCurrent_page(), result.getTotal_page());
+                                loading.dismiss();
+                            }
+                        });
             }
         });
     }
@@ -68,8 +79,9 @@ public class ReplyFragment extends BasicFragment {
      * 获取赞
      */
     private void getNotifyList() {
+        mPage=1;
         loading.show();
-        networkRequest(StartApi.getReplyList(String.valueOf(currentPage)),
+        networkRequest(StartApi.getReplyList(String.valueOf(mPage)),
                 new SimpleFastJsonCallback<Reply>(Reply.class, loading) {
                     @Override
                     public void onSuccess(String url, Reply result) {
@@ -77,18 +89,25 @@ public class ReplyFragment extends BasicFragment {
                         if (details != null && details.size() > 0) {
                             rl_no_message.setVisibility(View.GONE);
                             rv_content.setVisibility(View.VISIBLE);
-                            adapter.addAll(details);
-                        }else{
+                            adapter.replaceAll(details);
+                        } else {
                             rl_no_message.setVisibility(View.VISIBLE);
                             rv_content.setVisibility(View.GONE);
                         }
-                        if (result.getCurrent_page() != result.getTotal_page() && result.getTotal_page() != 0) {
+                       /* if (result.getCurrent_page() != result.getTotal_page() && result.getTotal_page() != 0) {
                             currentPage++;
                             rv_content.loadMoreComplete();
-                        } else rv_content.noMoreLoading();
+                        } else rv_content.noMoreLoading();*/
+                        checkLoadMoreComplete(result.getCurrent_page(), result.getTotal_page());
                         loading.dismiss();
                     }
                 });
+    }
+
+    private void checkLoadMoreComplete(int currentPage, int totalPage) {
+        if (currentPage == totalPage)
+            rv_content.noMoreLoading();
+        else mPage++;
     }
 
     @Override
