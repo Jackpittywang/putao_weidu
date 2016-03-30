b8f4041acb26cec3826574405deed059e340eeb0
zhanghao <zhangh@putao.com>
2016-03-30 14:16:09 +0800

    判断是否有网

--- app/src/main/java/com/putao/mtlib/util/NetManager.java
+++ app/src/main/java/com/putao/mtlib/util/NetManager.java
@@ -9,109 +9,130 @@ import android.net.NetworkInfo;
 
 /**
  * 判断网络连接状
- * 
- * @author wjh
  *
+ * @author wjh
  */
 public class NetManager {
-	static NetManager s_m = null;
+    static NetManager s_m = null;
 
-	private Context context;
+    private Context context;
 
-	private NetManager() {
+    private NetManager() {
 
-	}
+    }
 
-	public void init(Context ctx) {
-		context = ctx;
-	}
+    public void init(Context ctx) {
+        context = ctx;
+    }
 
-	public static synchronized NetManager instance() {
-		if (s_m == null) {
-			s_m = new NetManager();
-		}
-		return s_m;
-	}
+    public static synchronized NetManager instance() {
+        if (s_m == null) {
+            s_m = new NetManager();
+        }
+        return s_m;
+    }
 
-	/**
-	 * 判断是否有网络连
-	 * 
-	 * @return
-	 */
-	public boolean isNetworkConnected() {
-		if (context == null) {
-			return false;
-		}
-		ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-		if (connectivity == null) {
-			return false;
-		} else {
-			NetworkInfo[] info = connectivity.getAllNetworkInfo();
-			if (info != null) {
-				for (int i = 0; i < info.length; i++) {
-					if (info[i].getState() == NetworkInfo.State.CONNECTED) {
-						return true;
-					}
-				}
-			}
-		}
-		return false;
-	}
+    /**
+     * 判断是否有网络连
+     *
+     * @return
+     */
+    public boolean isNetworkConnected() {
+        if (context == null) {
+            return false;
+        }
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity == null) {
+            return false;
+        } else {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * 判断WIFI网络是否可用
-	 * 
-	 * @return
-	 */
-	public boolean isWifiConnected() {
-		if (context != null) {
-			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
-					.getSystemService(Context.CONNECTIVITY_SERVICE);
-			NetworkInfo mWiFiNetworkInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-			if (mWiFiNetworkInfo != null) {
-				return mWiFiNetworkInfo.isAvailable();
-			}
-		}
-		return false;
-	}
+    /**
+     * 检测当的网络（WLAN、3G/2G）状态
+     *
+     * @param context Context
+     * @return true 表示网络可用
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivity = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo info = connectivity.getActiveNetworkInfo();
+            if (info != null && info.isConnected()) {
+                // 当前网络是连接的
+                if (info.getState() == NetworkInfo.State.CONNECTED) {
+                    // 当前所连接的网络可用
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-	/**
-	 * 判断MOBILE网络是否可用
-	 * 
-	 * @return
-	 */
-	public boolean isMobileConnected() {
-		if (context != null) {
-			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
-					.getSystemService(Context.CONNECTIVITY_SERVICE);
-			NetworkInfo mMobileNetworkInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-			if (mMobileNetworkInfo != null) {
-				return mMobileNetworkInfo.isAvailable();
-			}
-		}
-		return false;
-	}
+    /**
+     * 判断WIFI网络是否可用
+     *
+     * @return
+     */
+    public boolean isWifiConnected() {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mWiFiNetworkInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            if (mWiFiNetworkInfo != null) {
+                return mWiFiNetworkInfo.isAvailable();
+            }
+        }
+        return false;
+    }
 
-	public int getConnectedType() {
-		if (context != null) {
-			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
-					.getSystemService(Context.CONNECTIVITY_SERVICE);
-			NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
-			if (mNetworkInfo != null && mNetworkInfo.isAvailable()) {
-				return mNetworkInfo.getType();
-			}
-		}
-		return -1;
-	}
+    /**
+     * 判断MOBILE网络是否可用
+     *
+     * @return
+     */
+    public boolean isMobileConnected() {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mMobileNetworkInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            if (mMobileNetworkInfo != null) {
+                return mMobileNetworkInfo.isAvailable();
+            }
+        }
+        return false;
+    }
 
-	public String getIP(String name) {
-		InetAddress address = null;
-		try {
-			address = InetAddress.getByName(name);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			System.out.println("获取失败");
-		}
-		return address.getHostAddress().toString();
-	}
+    public int getConnectedType() {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+            if (mNetworkInfo != null && mNetworkInfo.isAvailable()) {
+                return mNetworkInfo.getType();
+            }
+        }
+        return -1;
+    }
+
+    public String getIP(String name) {
+        InetAddress address = null;
+        try {
+            address = InetAddress.getByName(name);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.out.println("获取失败");
+        }
+        return address.getHostAddress().toString();
+    }
 }
