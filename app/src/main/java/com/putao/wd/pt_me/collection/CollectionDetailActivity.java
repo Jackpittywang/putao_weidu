434aa98c1dda3363dc12e3a5c84632f3594c98a6
zhanghao <zhangh@putao.com>
2016-04-05 15:33:33 +0800

    发现、我的、忘记密码

---
+++ app/src/main/java/com/putao/wd/pt_me/collection/CollectionDetailActivity.java
@@ -0,0 +1,25 @@
+package com.putao.wd.pt_me.collection;
+
+import android.os.Bundle;
+
+import com.sunnybear.library.controller.BasicFragmentActivity;
+
+/**
+ * Created by Administrator on 2016/4/5.
+ */
+public class CollectionDetailActivity extends BasicFragmentActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void onViewCreatedFinish(Bundle saveInstanceState) {
+
+    }
+
+    @Override
+    protected String[] getRequestUrls() {
+        return new String[0];
+    }
+}
---
+++ app/src/main/java/com/putao/wd/pt_me/participation/ParticipationDetailActivity.java
@@ -0,0 +1,26 @@
+package com.putao.wd.pt_me.participation;
+
+import android.os.Bundle;
+
+import com.sunnybear.library.controller.BasicFragmentActivity;
+
+/**
+ * 我的参与详情
+ * Created by Administrator on 2016/4/5.
+ */
+public class ParticipationDetailActivity extends BasicFragmentActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void onViewCreatedFinish(Bundle saveInstanceState) {
+
+    }
+
+    @Override
+    protected String[] getRequestUrls() {
+        return new String[0];
+    }
+}
