5e1b288421e2e5ed83f496dc4d8e770f79d6b145
zhanghao <zhanghao@putao.com>
2016-04-06 20:57:32 +0800

    游戏步骤列列表

--- app/src/main/java/com/putao/wd/pt_companion/GameStepDetailActivity.java
+++ app/src/main/java/com/putao/wd/pt_companion/GameStepDetailActivity.java
@@ -7,6 +7,7 @@ import android.text.style.ForegroundColorSpan;
 
 import com.putao.wd.R;
 import com.putao.wd.base.PTWDActivity;
+import com.sunnybear.library.controller.BasicFragmentActivity;
 import com.sunnybear.library.view.BasicWebView;
 
 import butterknife.Bind;
@@ -15,7 +16,7 @@ import butterknife.Bind;
  * 游戏详情页
  * Created by zhanghao on 2016/04/05.
  */
-public class GameStepDetailActivity extends PTWDActivity {
+public class GameStepDetailActivity extends BasicFragmentActivity {
     @Bind(R.id.wv_content)
     BasicWebView wv_content;
 
