3c469c00b7bc886216cd264d5d9b120011a62496
zhanghao <zhanghao@putao.com>
2016-04-06 18:27:35 +0800

    游戏步骤列列表

--- app/src/main/java/com/putao/wd/pt_companion/GameDetailActivity.java
+++ app/src/main/java/com/putao/wd/pt_companion/GameDetailActivity.java
@@ -1,12 +1,17 @@
 package com.putao.wd.pt_companion;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 
 import com.putao.wd.R;
 import com.putao.wd.base.PTWDActivity;
 import com.putao.wd.model.GameList;
 import com.putao.wd.pt_companion.adapter.GameStepListAdapter;
+import com.sunnybear.library.controller.BasicFragmentActivity;
 import com.sunnybear.library.controller.eventbus.Subcriber;
+import com.sunnybear.library.view.BasicWebView;
 import com.sunnybear.library.view.recycler.BasicRecyclerView;
 import com.sunnybear.library.view.recycler.listener.OnItemClickListener;
 
@@ -19,25 +24,19 @@ import butterknife.Bind;
  * 游戏详情页
  * Created by zhanghao on 2016/04/05.
  */
-public class GameDetailActivity extends PTWDActivity{
-    private GameStepListAdapter mGameStepListAdapter;
-    @Bind(R.id.rv_content)
-    BasicRecyclerView rv_content;
+public class GameDetailActivity extends PTWDActivity {
+    @Bind(R.id.wv_content)
+    BasicWebView wv_content;
 
     @Override
     protected int getLayoutId() {
-        return R.layout.activity_game_step_list;
+        return R.layout.activity_game_detail;
     }
 
     @Override
     public void onViewCreatedFinish(Bundle savedInstanceState) {
-        ArrayList<GameList> gameLists = new ArrayList<>();
-        gameLists.add(new GameList());
-        gameLists.add(new GameList());
-        gameLists.add(new GameList());
-        gameLists.add(new GameList());
-        mGameStepListAdapter = new GameStepListAdapter(mContext, gameLists);
-        rv_content.setAdapter(mGameStepListAdapter);
+        SpannableString ss = new SpannableString("淘淘向右走" + "关卡地图");
+        ss.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.text_color_gray)), ss.length() - 4, ss.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
     }
 
     @Override
--- app/src/main/java/com/putao/wd/pt_companion/GameStepListActivity.java
+++ app/src/main/java/com/putao/wd/pt_companion/GameStepListActivity.java
@@ -1,25 +1,38 @@
 package com.putao.wd.pt_companion;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.putao.wd.R;
 import com.putao.wd.base.PTWDActivity;
 import com.putao.wd.model.GameList;
 import com.putao.wd.pt_companion.adapter.GameStepListAdapter;
+import com.sunnybear.library.controller.BasicFragmentActivity;
 import com.sunnybear.library.controller.eventbus.Subcriber;
 import com.sunnybear.library.view.recycler.BasicRecyclerView;
 import com.sunnybear.library.view.recycler.listener.OnItemClickListener;
+import com.sunnybear.library.view.recycler.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
+import com.sunnybear.library.view.recycler.stickyheadersrecyclerview.StickyRecyclerHeadersTouchListener;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
 import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * 游戏步骤选择页
  * Created by zhanghao on 2016/04/05.
  */
-public class GameStepListActivity extends PTWDActivity {
+public class GameStepListActivity extends BasicFragmentActivity implements View.OnClickListener {
     private GameStepListAdapter mGameStepListAdapter;
     @Bind(R.id.rv_content)
     BasicRecyclerView rv_content;
@@ -32,14 +45,61 @@ public class GameStepListActivity extends PTWDActivity {
     @Override
     public void onViewCreatedFinish(Bundle savedInstanceState) {
         ArrayList<GameList> gameLists = new ArrayList<>();
-        gameLists.add(new GameList());
-        gameLists.add(new GameList());
-        gameLists.add(new GameList());
-        gameLists.add(new GameList());
+        for (int i = 0; i < 100; i++) {
+            gameLists.add(new GameList());
+        }
         mGameStepListAdapter = new GameStepListAdapter(mContext, gameLists);
         rv_content.setAdapter(mGameStepListAdapter);
+
+
+        // Set layout manager
+        int orientation = getLayoutManagerOrientation(getResources().getConfiguration().orientation);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this, orientation, false);
+        rv_content.setLayoutManager(layoutManager);
+
+        // Add the sticky headers decoration
+        final StickyRecyclerHeadersDecoration headersDecor = new StickyRecyclerHeadersDecoration(mGameStepListAdapter);
+        rv_content.addItemDecoration(headersDecor);
+
+        // Add decoration for dividers between list items
+        rv_content.addItemDecoration(new DividerDecoration(this));
+
+        // Add touch listeners
+        StickyRecyclerHeadersTouchListener touchListener =
+                new StickyRecyclerHeadersTouchListener(rv_content, headersDecor);
+        touchListener.setOnHeaderClickListener(
+                new StickyRecyclerHeadersTouchListener.OnHeaderClickListener() {
+                    @Override
+                    public void onHeaderClick(View header, int position, long headerId) {
+                        Toast.makeText(mContext, "Header position: " + position + ", id: " + headerId,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        rv_content.addOnItemTouchListener(touchListener);
+        rv_content.addOnItemTouchListener(new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+//                startActivity(GameDetailActivity.class);
+                mGameStepListAdapter.notifyItemRemoved(position);
+//                mGameStepListAdapter.remove(mGameStepListAdapter.getItem(position));
+            }
+        }));
+        mGameStepListAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onChanged() {
+                headersDecor.invalidateHeaders();
+            }
+        });
+
     }
 
+    private int getLayoutManagerOrientation(int activityOrientation) {
+        if (activityOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+            return LinearLayoutManager.VERTICAL;
+        } else {
+            return LinearLayoutManager.HORIZONTAL;
+        }
+    }
 
     @Override
     protected String[] getRequestUrls() {
@@ -50,6 +110,55 @@ public class GameStepListActivity extends PTWDActivity {
     private void startDetail(String str) {
         startActivity(GameDetailActivity.class);
     }
+
+    @OnClick(R.id.iv_back)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+        }
+    }
+
+    static class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+        private OnItemClickListener mListener;
+
+        public interface OnItemClickListener {
+            void onItemClick(View view, int position);
+        }
+
+        GestureDetector mGestureDetector;
+
+        public RecyclerItemClickListener(Context context, OnItemClickListener listener) {
+            mListener = listener;
+            mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e) {
+            View childView = view.findChildViewUnder(e.getX(), e.getY());
+            if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+                mListener.onItemClick(childView, view.getChildAdapterPosition(childView));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) {
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+            // do nothing
+        }
+    }
+
 }
 
 
