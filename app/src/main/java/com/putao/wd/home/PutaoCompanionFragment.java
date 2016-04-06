fb63c75e7f3f5f4773cfd4ca2ea639caf41311ae
zhanghao <zhanghao@putao.com>
2016-04-06 11:39:47 +0800

    陪伴测试

--- app/src/main/java/com/putao/wd/home/PutaoCompanionFragment.java
+++ app/src/main/java/com/putao/wd/home/PutaoCompanionFragment.java
@@ -1,31 +1,26 @@
 package com.putao.wd.home;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
+import android.os.Handler;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.putao.wd.R;
-import com.putao.wd.account.YouMengHelper;
-import com.putao.wd.api.StoreApi;
 import com.putao.wd.base.PTWDFragment;
 import com.putao.wd.home.adapter.CompanionAdapter;
-import com.putao.wd.home.adapter.StoreAdapter;
 import com.putao.wd.model.Companion;
-import com.putao.wd.model.StoreProduct;
-import com.putao.wd.model.StoreProductHome;
 import com.putao.wd.pt_companion.BlackboardActivity;
-import com.putao.wd.pt_store.product.ProductDetailActivity;
-import com.sunnybear.library.controller.BasicFragment;
-import com.sunnybear.library.model.http.callback.SimpleFastJsonCallback;
-import com.sunnybear.library.util.Logger;
-import com.sunnybear.library.view.PullToRefreshLayout;
-import com.sunnybear.library.view.SettingItem;
+import com.putao.wd.pt_companion.GameDetailActivity;
 import com.sunnybear.library.view.recycler.LoadMoreRecyclerView;
-import com.sunnybear.library.view.recycler.animators.ScaleInAnimation;
 import com.sunnybear.library.view.recycler.listener.OnItemClickListener;
+import com.sunnybear.library.view.scroll.NestScrollView;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.Bind;
 
@@ -37,8 +32,12 @@ public class PutaoCompanionFragment extends PTWDFragment implements OnItemClickL
     private CompanionAdapter mCompanionAdapter;
     @Bind(R.id.rv_content)
     LoadMoreRecyclerView rv_content;
-    @Bind(R.id.ptl_refresh)
-    PullToRefreshLayout ptl_refresh;
+    @Bind(R.id.wv_load)
+    WebView wv_load;
+    @Bind(R.id.sv_load)
+    NestScrollView sv_load;
+    /*@Bind(R.id.rl_load)
+    LinearLayout rl_load;*/
 
     @Override
     protected int getLayoutId() {
@@ -47,30 +46,49 @@ public class PutaoCompanionFragment extends PTWDFragment implements OnItemClickL
 
     @Override
     public void onViewCreatedFinish(Bundle savedInstanceState) {
+        wv_load.loadDataWithBaseURL("about:blank", "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhnfdfdfdsfsdf", "text/html", "utf-8", null);
+//        wv_load.loadUrl("http://www.linuxidc.com/Linux/2014-03/98847.htm");
+        addListener();
+    }
+
+    private void addListener() {
+
+       /* rv_content.setOnLoadMoreListener(new LoadMoreRecyclerView.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                rv_content.noMoreLoading();
+            }
+        });*/
         ArrayList<Companion> companions = new ArrayList<>();
+        companions.add(new Companion());
+        companions.add(new Companion());
+        companions.add(new Companion());
+        companions.add(new Companion());
+        companions.add(new Companion());
         companions.add(new Companion());
         companions.add(new Companion());
         companions.add(new Companion());
         companions.add(new Companion());
         mCompanionAdapter = new CompanionAdapter(mActivity, companions);
         rv_content.setAdapter(mCompanionAdapter);
-        addListener();
-    }
-
-    private void addListener() {
-        ptl_refresh.setOnRefreshListener(new PullToRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                ptl_refresh.refreshComplete();
-            }
-        });
-        rv_content.setOnLoadMoreListener(new LoadMoreRecyclerView.OnLoadMoreListener() {
-            @Override
-            public void onLoadMore() {
-                rv_content.noMoreLoading();
-            }
-        });
         mCompanionAdapter.setOnItemClickListener(this);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                /*ViewGroup.LayoutParams layoutParams1 = sv_load.getLayoutParams();
+                layoutParams1.height = 3000;
+                sv_load.setLayoutParams(layoutParams1);*/
+               /* ViewGroup.LayoutParams layoutParams2 = rl_load.getLayoutParams();
+                layoutParams2.height = 3000;
+                rl_load.setLayoutParams(layoutParams2);*/
+            }
+        }, 2000);
+        /*ViewGroup.LayoutParams layoutParams = wv_load.getLayoutParams();
+        layoutParams.height = 1000;
+        wv_load.setLayoutParams(layoutParams);*/
+
+
     }
 
     @Override
@@ -81,6 +99,7 @@ public class PutaoCompanionFragment extends PTWDFragment implements OnItemClickL
     @Override
     public void onItemClick(Serializable serializable, int position) {
         if (0 == position) startActivity(BlackboardActivity.class);
+        else startActivity(GameDetailActivity.class);
     }
 }
 
---
+++ app/src/main/java/com/putao/wd/pt_companion/GameDetailActivity.java
@@ -0,0 +1,73 @@
+package com.putao.wd.pt_companion;
+
+import android.os.Bundle;
+
+import com.putao.wd.R;
+import com.putao.wd.base.PTWDActivity;
+import com.putao.wd.home.adapter.CompanionAdapter;
+import com.putao.wd.model.Companion;
+import com.sunnybear.library.view.PullToRefreshLayout;
+import com.sunnybear.library.view.recycler.LoadMoreRecyclerView;
+import com.sunnybear.library.view.recycler.listener.OnItemClickListener;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+
+/**
+ * 游戏详情页
+ * Created by zhanghao on 2016/04/05.
+ */
+public class GameDetailActivity extends PTWDActivity implements OnItemClickListener {
+    private CompanionAdapter mCompanionAdapter;
+    @Bind(R.id.rv_content)
+    LoadMoreRecyclerView rv_content;
+    @Bind(R.id.ptl_refresh)
+    PullToRefreshLayout ptl_refresh;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_game_detail;
+    }
+
+    @Override
+    public void onViewCreatedFinish(Bundle savedInstanceState) {
+        ArrayList<Companion> companions = new ArrayList<>();
+        companions.add(new Companion());
+        companions.add(new Companion());
+        companions.add(new Companion());
+        companions.add(new Companion());
+        mCompanionAdapter = new CompanionAdapter(mContext, companions);
+        rv_content.setAdapter(mCompanionAdapter);
+        addListener();
+    }
+
+    private void addListener() {
+        ptl_refresh.setOnRefreshListener(new PullToRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                ptl_refresh.refreshComplete();
+            }
+        });
+        rv_content.setOnLoadMoreListener(new LoadMoreRecyclerView.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                rv_content.noMoreLoading();
+            }
+        });
+        mCompanionAdapter.setOnItemClickListener(this);
+    }
+
+    @Override
+    protected String[] getRequestUrls() {
+        return new String[0];
+    }
+
+    @Override
+    public void onItemClick(Serializable serializable, int position) {
+
+    }
+}
+
+
