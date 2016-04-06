d6c4045b6aa9c7728bbd963b1114e147179eb925
zhanghao <zhangh@putao.com>
2016-04-06 17:36:14 +0800

    我的收藏

--- app/src/main/java/com/putao/wd/pt_me/collection/CollectionActivity.java
+++ app/src/main/java/com/putao/wd/pt_me/collection/CollectionActivity.java
@@ -3,10 +3,6 @@ package com.putao.wd.pt_me.collection;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.view.MotionEventCompat;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -14,20 +10,15 @@ import com.putao.wd.R;
 import com.putao.wd.account.YouMengHelper;
 import com.putao.wd.api.CreateApi;
 import com.putao.wd.base.PTWDActivity;
-import com.putao.wd.created.adapter.FancyAdapter;
-import com.putao.wd.model.Collection;
 import com.putao.wd.model.Create;
 import com.putao.wd.model.Creates;
-import com.putao.wd.pt_me.participation.ParticipationAdapter;
 import com.sunnybear.library.model.http.callback.SimpleFastJsonCallback;
-import com.sunnybear.library.util.Logger;
 import com.sunnybear.library.view.PullToRefreshLayout;
 import com.sunnybear.library.view.recycler.LoadMoreRecyclerView;
 import com.sunnybear.library.view.recycler.listener.OnItemClickListener;
 import com.sunnybear.library.view.recycler.listener.OnItemLongClickListener;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -36,8 +27,7 @@ import butterknife.Bind;
  * 我的收藏
  * Created by Administrator on 2016/4/5.
  */
-public class CollectionActivity extends PTWDActivity implements OnItemClickListener, View.OnClickListener {
-    //    PullToRefreshLayout.OnRefreshListener, LoadMoreRecyclerView.OnLoadMoreListener,
+public class CollectionActivity extends PTWDActivity implements PullToRefreshLayout.OnRefreshListener, LoadMoreRecyclerView.OnLoadMoreListener, OnItemClickListener<Create>, View.OnClickListener {
     @Bind(R.id.ptl_refresh)
     PullToRefreshLayout ptl_refresh;
     @Bind(R.id.rv_collection)
@@ -59,128 +49,70 @@ public class CollectionActivity extends PTWDActivity implements OnItemClickListe
     @Override
     protected void onViewCreatedFinish(Bundle saveInstanceState) {
         addNavigation();
-        ArrayList<Collection> list = new ArrayList<>();
-        list.add(new Collection());
-        adapter = new CollectionAdapter(this, list);
+        adapter = new CollectionAdapter(this, null);
         rv_collection.setAdapter(adapter);
-//        initData();
+        initData();
         addListenter();
     }
-//
-//    private void initData() {
-//        mPage = 1;
-//        networkRequest(CreateApi.getCreateMyfollows(mPage),
-//                new SimpleFastJsonCallback<Creates>(Creates.class, loading) {
-//                    @Override
-//                    public void onSuccess(String url, Creates result) {
-//                        List<Create> details = result.getData();
-//                        if (details != null && details.size() > 0) {
-//                            isCollection = false;
-//                            ll_empty.setVisibility(View.GONE);
-//                            ptl_refresh.setVisibility(View.VISIBLE);
-//                            adapter.replaceAll(details);
-//                        } else {
-//                            ptl_refresh.setVisibility(View.GONE);
-//                            ll_empty.setVisibility(View.VISIBLE);
-//                        }
-//                        checkLoadMoreComplete(result.getCurrentPage(), result.getTotalPage());
-//                        ptl_refresh.refreshComplete();
-//                        loading.dismiss();
-//
-//                        /*adapter.replaceAll(result.getData());
-//                        checkLoadMoreComplete(result.getCurrentPage(), result.getTotalPage());
-//                        loading.dismiss();*/
-//                    }
-//                });
-//    }
 
-//    private void checkLoadMoreComplete(int currentPage, int totalPage) {
-//        if (currentPage == totalPage) {
-//            rv_participation.noMoreLoading();
-//            hasMoreData = false;
-//        } else {
-//            mPage++;
-//            hasMoreData = true;
-//        }
-//    }
+    private void initData() {
+        mPage = 1;
+        networkRequest(CreateApi.getCreateMyfollows(mPage),
+                new SimpleFastJsonCallback<Creates>(Creates.class, loading) {
+                    @Override
+                    public void onSuccess(String url, Creates result) {
+                        List<Create> details = result.getData();
+                        if (details != null && details.size() > 0) {
+                            isCollection = false;
+                            ll_empty.setVisibility(View.GONE);
+                            ptl_refresh.setVisibility(View.VISIBLE);
+                            adapter.replaceAll(details);
+                        } else {
+                            ptl_refresh.setVisibility(View.GONE);
+                            ll_empty.setVisibility(View.VISIBLE);
 
-//    private void addListenter() {
-//        rv_participation.setOnItemClickListener(this);
-//        ll_empty.setOnClickListener(this);
-//        ptl_refresh.setOnRefreshListener(this);
-//        rv_participation.setOnLoadMoreListener(this);
-//        rv_participation.setOnItemLongClickListener(new OnItemLongClickListener() {
-//            @Override
-//            public void onItemLongClick(Serializable serializable, int position) {
-//                mDeleteDialog = new AlertDialog.Builder(mContext)
-//                        .setTitle("删除收藏")
-//                        .setMessage("是否删除该条收藏数据")
-//                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                networkRequest(CreateApi.setCreateAction(mCreate.getId(), 3),
-//                                        new SimpleFastJsonCallback<String>(String.class, loading) {
-//                                            @Override
-//                                            public void onSuccess(String url, String result) {
-//                                                mCreate.setFollow_status(1);
-//                                                Logger.d(result.toString());
-////                                        adapter.delete();
-////                                        if (adapter.getItemCount() == 0) isCollection = true;
-////                                        eventAddressDelete(address.getId());
-////                                        getAddressLists();
-//                                            }
-//                                        });
-//
-//                            }
-//                        }).setNeutralButton("取消", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                mDeleteDialog.dismiss();
-//                            }
-//                        }).show();
-//            }
-//        });
-//    }
-
-    @Override
-    protected String[] getRequestUrls() {
-        return new String[0];
+                        }
+                        checkLoadMoreComplete(result.getCurrentPage(), result.getTotalPage());
+                        ptl_refresh.refreshComplete();
+                        loading.dismiss();
+                    }
+                }
+        );
     }
 
-    @Override
-    public void onClick(View v) {
 
+    private void checkLoadMoreComplete(int currentPage, int totalPage) {
+        if (currentPage == totalPage) {
+            rv_collection.noMoreLoading();
+            hasMoreData = false;
+        } else {
+            mPage++;
+            hasMoreData = true;
+        }
     }
 
-    @Override
-    public void onItemClick(Serializable serializable, int position) {
-        Bundle bundle = new Bundle();
-        startActivity(CollectionDetailActivity.class, bundle);
-    }
-
-    public void addListenter() {
-        ptl_refresh.setOnRefreshListener(new PullToRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                ptl_refresh.refreshComplete();
-            }
-        });
-        rv_collection.setOnLoadMoreListener(new LoadMoreRecyclerView.OnLoadMoreListener() {
-            @Override
-            public void onLoadMore() {
-                rv_collection.loadMoreComplete();
-            }
-        });
+    private void addListenter() {
+        rv_collection.setOnItemClickListener(this);
+        ll_empty.setOnClickListener(this);
+        ptl_refresh.setOnRefreshListener(this);
+        rv_collection.setOnLoadMoreListener(this);
         rv_collection.setOnItemLongClickListener(new OnItemLongClickListener() {
             @Override
             public void onItemLongClick(Serializable serializable, int position) {
                 mDeleteDialog = new AlertDialog.Builder(mContext)
 //                        .setTitle("删除收藏数据")
-                        .setMessage("确定删除所选的收藏项?")
+                        .setMessage("确定删除所选的收藏项？")
                         .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-
+                                networkRequest(CreateApi.setCreateAction(mCreate.getId(), 3),
+                                        new SimpleFastJsonCallback<String>(String.class, loading) {
+                                            @Override
+                                            public void onSuccess(String url, String result) {
+                                                mCreate.setFollow_status(1);
+//                                                adapter.replaceAll();
+                                            }
+                                        });
                             }
                         })
                         .setNegativeButton("取消", new DialogInterface.OnClickListener() {
@@ -192,49 +124,47 @@ public class CollectionActivity extends PTWDActivity implements OnItemClickListe
                         }).show();
             }
         });
-
-        rv_collection.setOnItemClickListener(this);
-        rv_collection.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return false;
-            }
-        });
     }
 
 
-//    @Override
-//    public void onRefresh() {
-//        initData();
-//    }
+    @Override
+    protected String[] getRequestUrls() {
+        return new String[0];
+    }
 
-//    @Override
-//    public void onLoadMore() {
-//        networkRequest(CreateApi.getCreateMyfollows(mPage),
-//                new SimpleFastJsonCallback<Creates>(Creates.class, loading) {
-//                    @Override
-//                    public void onSuccess(String url, Creates result) {
-//                        adapter.addAll(result.getData());
-//                        rv_participation.loadMoreComplete();
-//                        checkLoadMoreComplete(result.getCurrentPage(), result.getTotalPage());
-//                        loading.dismiss();
-//                    }
-//                });
-//    }
-//
-//    private Create mCreate;
-//
-//    @Override
-//    public void onItemClick(Create create, int position) {
-//        mCreate = create;
-//        Bundle bundle = new Bundle();
-//        bundle.putSerializable(CollectionDetailActivity.CREATE, (Serializable) adapter.getItems());
-//        bundle.putInt(CollectionDetailActivity.POSITION, position);
-//        bundle.putInt(CollectionDetailActivity.PAGE_COUNT, mPage);
-//        bundle.putBoolean(CollectionDetailActivity.HAS_MORE_DATA, hasMoreData);
-//        YouMengHelper.onEvent(mContext, YouMengHelper.UserHome_interested_detail);
-//        startActivity(CollectionDetailActivity.class, bundle);
-//    }
+
+    @Override
+    public void onRefresh() {
+        initData();
+    }
+
+    @Override
+    public void onLoadMore() {
+        networkRequest(CreateApi.getCreateMyfollows(mPage),
+                new SimpleFastJsonCallback<Creates>(Creates.class, loading) {
+                    @Override
+                    public void onSuccess(String url, Creates result) {
+                        adapter.addAll(result.getData());
+                        rv_collection.loadMoreComplete();
+                        checkLoadMoreComplete(result.getCurrentPage(), result.getTotalPage());
+                        loading.dismiss();
+                    }
+                });
+    }
+
+    private Create mCreate;
+
+    @Override
+    public void onItemClick(Create create, int position) {
+        mCreate = create;
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(CollectionDetailActivity.CREATE, (Serializable) adapter.getItems());
+        bundle.putInt(CollectionDetailActivity.POSITION, position);
+        bundle.putInt(CollectionDetailActivity.PAGE_COUNT, mPage);
+        bundle.putBoolean(CollectionDetailActivity.HAS_MORE_DATA, hasMoreData);
+        YouMengHelper.onEvent(mContext, YouMengHelper.UserHome_interested_detail);
+        startActivity(CollectionDetailActivity.class, bundle);
+    }
 
 //    @Subcriber(tag = CreateBasicDetailActivity.EVENT_CONCERNS_REFRESH)
 //    private void eventRefresh(String str) {
@@ -283,14 +213,14 @@ public class CollectionActivity extends PTWDActivity implements OnItemClickListe
 //        }
 //    }
 
-//    @Override
-//    public void onClick(View v) {
-//        initData();
-//    }
+    @Override
+    public void onClick(View v) {
+        initData();
+    }
 
-//    @Override
-//    public void onLeftAction() {
-//        super.onLeftAction();
-//        YouMengHelper.onEvent(mContext, YouMengHelper.UserHome_interested_back);
-//    }
+    @Override
+    public void onLeftAction() {
+        super.onLeftAction();
+        YouMengHelper.onEvent(mContext, YouMengHelper.UserHome_interested_back);
+    }
 }
--- app/src/main/java/com/putao/wd/pt_me/collection/CollectionAdapter.java
+++ app/src/main/java/com/putao/wd/pt_me/collection/CollectionAdapter.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import com.putao.wd.R;
 import com.putao.wd.model.Collection;
+import com.putao.wd.model.Create;
 import com.sunnybear.library.view.image.ImageDraweeView;
 import com.sunnybear.library.view.recycler.BasicViewHolder;
 import com.sunnybear.library.view.recycler.adapter.LoadMoreAdapter;
@@ -23,10 +24,10 @@ import butterknife.Bind;
  * 我的收藏的适配器
  * Created by Administrator on 2016/4/6.
  */
-public class CollectionAdapter extends LoadMoreAdapter<Collection, CollectionAdapter.CollectionViewHodler> {
+public class CollectionAdapter extends LoadMoreAdapter<Create, CollectionAdapter.CollectionViewHodler> {
 
-    public CollectionAdapter(Context context, List<Collection> collections) {
-        super(context, collections);
+    public CollectionAdapter(Context context, List<Create> creates) {
+        super(context, creates);
     }
 
     @Override
@@ -40,17 +41,10 @@ public class CollectionAdapter extends LoadMoreAdapter<Collection, CollectionAda
     }
 
     @Override
-    public void onBindItem(final CollectionViewHodler holder, Collection collection, int position) {
-//        View contentView = LayoutInflater.from(context).inflate(R.layout.activity_collection_bgmenu, null);
-//        contentView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
-//        TextView tv_delete = (TextView) contentView.findViewById(R.id.tv_delete);
-//        tv_delete.setOnClickListener(new View.OnClickListener() {//删除
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-        holder.tv_title.setText("收藏" + position);
+    public void onBindItem(final CollectionViewHodler holder, Create creates, int position) {
+        holder.tv_title.setText(creates.getTitle());
+        holder.tv_content.setText(creates.getDescrip());
+        holder.iv_icon.setImageURL(creates.getSmall_cover());
     }
 
     static class CollectionViewHodler extends BasicViewHolder {
