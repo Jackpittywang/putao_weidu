0118b7d7d48912554c567df4cebc8f5f3b74e640
zhanghao <zhanghao@putao.com>
2016-04-14 13:34:38 +0800

    分享

--- app/src/main/java/com/putao/wd/SplashActivity.java
+++ app/src/main/java/com/putao/wd/SplashActivity.java
@@ -81,6 +81,7 @@ public class SplashActivity extends BasicFragmentActivity {
                             checkNotify();
                             startActivity(IndexActivity.class);
                         }
+                        finish();
                     }
                 }).execute();
 //        else
