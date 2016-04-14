73d341734db29229e2d32f44a9ac98a39b81b6d2
zhanghao <zhangh@putao.com>
2016-04-14 19:22:11 +0800

    修改商品图片

--- app/src/main/java/com/putao/wd/home/adapter/StoreAdapter.java
+++ app/src/main/java/com/putao/wd/home/adapter/StoreAdapter.java
@@ -36,7 +36,7 @@ public class StoreAdapter extends LoadMoreAdapter<StoreProduct, StoreAdapter.Sto
 
     @Override
     public void onBindItem(StoreViewHolder holder, StoreProduct product, int position) {
-        holder.iv_product/*.addProcessor(new BlurProcessor(8))*/.setImageURL(product.getIcon());
+        holder.iv_product/*.addProcessor(new BlurProcessor(8))*/.setImageURL(product.getImage());
         holder.tv_title.setText(product.getTitle());
     }
 
