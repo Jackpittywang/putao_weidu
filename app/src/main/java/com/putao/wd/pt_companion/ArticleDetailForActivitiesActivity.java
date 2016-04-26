0d265919feb2fa39f88dc8439b6011512910c47b
huli <hul@putao.com>
2016-04-26 13:32:58 +0800

    文章分享图片不固定大小-修改

--- app/src/main/java/com/putao/wd/pt_companion/ArticleDetailForActivitiesActivity.java
+++ app/src/main/java/com/putao/wd/pt_companion/ArticleDetailForActivitiesActivity.java
@@ -179,26 +179,26 @@ public class ArticleDetailForActivitiesActivity extends BaseWebViewActivity impl
 
 
         mSharePopupWindow.setOnShareClickListener(new OnShareClickListener() {
-            String image = ImageUtils.getImageSizeUrl(cover_pic, ImageUtils.ImageSizeURL.SIZE_120x120);
+//            String image = ImageUtils.getImageSizeUrl(cover_pic, ImageUtils.ImageSizeURL.SIZE_120x120);
 
             @Override
             public void onWechat() {
-                ShareTools.wechatWebShare(mContext, true, title, sub_title, image, link_url);
+                ShareTools.wechatWebShare(mContext, true, title, sub_title, cover_pic, link_url);
             }
 
             @Override
             public void onWechatFriend() {
-                ShareTools.wechatWebShare(mContext, false, title, sub_title, image, link_url);
+                ShareTools.wechatWebShare(mContext, false, title, sub_title, cover_pic, link_url);
             }
 
             @Override
             public void onQQFriend() {
-                ShareTools.OnQQZShare(mContext, true, title, sub_title, image, link_url);
+                ShareTools.OnQQZShare(mContext, true, title, sub_title, cover_pic, link_url);
             }
 
             @Override
             public void onQQZone() {
-                ShareTools.OnQQZShare(mContext, false, title, sub_title, image, link_url);
+                ShareTools.OnQQZShare(mContext, false, title, sub_title, cover_pic, link_url);
             }
 
             public void onSinaWeibo() {
