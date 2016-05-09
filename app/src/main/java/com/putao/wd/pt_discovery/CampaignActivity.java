c90e4694dec11ee399fb8206f6cd814ee046dfbf
huli <hul@putao.com>
2016-05-09 15:11:33 +0800

    New changelist

--- app/src/main/java/com/putao/wd/pt_discovery/CampaignActivity.java
+++ app/src/main/java/com/putao/wd/pt_discovery/CampaignActivity.java
@@ -10,10 +10,13 @@ import com.putao.wd.model.DisCovery;
 import com.putao.wd.model.Resources;
 import com.putao.wd.model.SubscribeList;
 import com.putao.wd.pt_discovery.adapter.CampaignAdapter;
+import com.putao.wd.webview.BaseWebViewActivity;
 import com.sunnybear.library.model.http.callback.SimpleFastJsonCallback;
 import com.sunnybear.library.view.PullToRefreshLayout;
 import com.sunnybear.library.view.recycler.LoadMoreRecyclerView;
+import com.sunnybear.library.view.recycler.listener.OnItemClickListener;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,7 +47,6 @@ public class CampaignActivity extends PTWDActivity {
         campaignAdapter = new CampaignAdapter(mContext, null);
         rv_content.setAdapter(campaignAdapter);
         addListener();
-        System.out.println("========================" + args.getString(TAGID));
         getTagCampaign(args.getString(TAGID));
     }
 
@@ -63,9 +65,20 @@ public class CampaignActivity extends PTWDActivity {
                 rv_content.noMoreLoading();
             }
         });
+
+        rv_content.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(Serializable serializable, int position) {
+                Bundle bundle = new Bundle();
+                bundle.putString(BaseWebViewActivity.TITLE, resources.get(position).getTitle());
+                bundle.putString(BaseWebViewActivity.SERVICE_ID, resources.get(position).getSid());
+                bundle.putString(BaseWebViewActivity.URL, resources.get(position).getLink_url());
+                startActivity(BaseWebViewActivity.class, bundle);
+            }
+        });
     }
 
-    private void getTagCampaign(String tagId) {//String.valueOf(tagId)
+    private void getTagCampaign(String tagId) {
         networkRequest(DisCoveryApi.getTagResources(String.valueOf(tagId)), new SimpleFastJsonCallback<Campaign>(Campaign.class, loading) {
             @Override
             public void onSuccess(String url, Campaign result) {
