40f6592f0b5dbc73f059e938784b8783c8935646
yanguoqiang <yangq@putao.com>
2016-05-12 15:58:29 +0800

    同一gpush key和tokey

--- app/src/main/java/com/putao/ptx/push/core/Constants.java
+++ app/src/main/java/com/putao/ptx/push/core/Constants.java
@@ -9,12 +9,18 @@ public class Constants {
     public static final String EXTRA_PAYLOAD = "payload";
 
     public static final String DEFAULT_SERVER_DEBUG = "http://10.1.11.171:10090/mqtt_server";
-    public static final String DEFAULT_SERVER = "http://10.1.11.171:10090/mqtt_server";
+    public static final String DEFAULT_SERVER = "http://push-gateway.putaocloud.com/mqtt_server";
 
     public static final String PLATFORM = "android";
 
-    public static final String GPUSH_KEY = "0d87e77f509a419285db58f985836901";
-    public static final String GPUSH_TOKEN = "2fa77ec72e6a4c338515bfef98b97c42";
+    //public static final String GPUSH_APP_ID_DEBUG = "15";
+    //public static final String GPUSH_APP_ID = "1009";
+
+    public static final String GPUSH_KEY_DEBUG = "1234567890";
+    public static final String GPUSH_KEY = "47bbc691175b11e69411005056c00000";
+
+    public static final String GPUSH_TOKEN_DEBUG = "asdfghjkl";
+    public static final String GPUSH_TOKEN = "c4d7d7870fde40f4b054459bf1e72957";
 
     public static final String EXTRA_ACTION = "action";
     public static final int ACTION_GPUSH_START = 0;
@@ -22,24 +28,37 @@ public class Constants {
 
     public static String DEVICE_ID = "";
 
-    public static String APP_ID = "";
 
+    public static String APP_ID = "";
 
     public static boolean isDebug = true;
 
     /**
-     * 设置deviceid 和 appid
+     * 设置deviceid, appid 是否是debug
+     *
      * @param deviceId
      * @param appId
+     * @param isdebug
      */
-    public static void setDeviceAndAppId(String deviceId, String appId, boolean isDebug){
+    public static void setDeviceAndAppId(String deviceId, String appId, boolean isdebug) {
         DEVICE_ID = deviceId;
         APP_ID = appId;
+        isDebug = isdebug;
     }
 
-    public static String getServerUrl(){
-        if(isDebug) return DEFAULT_SERVER_DEBUG;
+    public static String getServerUrl() {
+        if (isDebug) return DEFAULT_SERVER_DEBUG;
         else return DEFAULT_SERVER;
+    }
+
+    public static String getPushKey() {
+        if (isDebug) return GPUSH_KEY_DEBUG;
+        else return GPUSH_KEY;
+    }
+
+    public static String getPushToken() {
+        if (isDebug) return GPUSH_TOKEN_DEBUG;
+        else return GPUSH_TOKEN_DEBUG;
     }
 
 }
