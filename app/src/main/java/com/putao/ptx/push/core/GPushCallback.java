b24f31643b7644468479bc942c946106af8eaf41
yanguoqiang <yangq@putao.com>
2016-05-17 19:12:17 +0800

    add gpush log file

--- app/src/main/java/com/putao/ptx/push/core/GPushCallback.java
+++ app/src/main/java/com/putao/ptx/push/core/GPushCallback.java
@@ -2,7 +2,19 @@ package com.putao.ptx.push.core;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.util.Log;
+
+import com.youku.analytics.utils.FileOperation;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
@@ -22,8 +34,68 @@ public class GPushCallback {
             i.putExtra(Constants.EXTRA_PAYLOAD, payload);
             if (mContext != null) mContext.sendBroadcast(i);
             Log.d("GPUSH", "send gpush broadcast");
+            String fpath = Environment.getExternalStorageDirectory().getPath()+"/gushlog.txt";
+            saveLog(fpath, payload);
         }
 
+    }
+
+    /**
+     * 保存推送日志到sd卡gpushlig.txt
+     * @param fPath
+     * @param str
+     */
+    public void saveLog(String fPath, String str) {
+        File file = new File(fPath);
+        String contentstr = "";
+        if (file.exists() == true) {
+            contentstr = readFile(fPath);
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateString = formatter.format(new Date(System
+                .currentTimeMillis()));
+        str = dateString + " " + str;
+        contentstr = str + "\n" + contentstr;
+        if (contentstr.length() > 10000)
+            contentstr = contentstr.substring(0, 10000);
+        saveFile(fPath, contentstr);
+        formatter = null;
+    }
+
+    public static void saveFile(String fileName, String str) {
+        File file = new File(fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            byte[] bytes = str.getBytes();
+            fos.write(bytes);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readFile(String fileName) {
+        String res = "";
+        try {
+            File fileDir = new File(fileName);
+            FileInputStream is = new FileInputStream(fileDir);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] array = new byte[1024];
+            int len = -1;
+
+            while ((len = is.read(array)) != -1) {
+                bos.write(array, 0, len);
+            }
+            bos.close();
+            is.close();
+            res = bos.toString();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
 
     }
 }
